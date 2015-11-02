083275b062381fa101100de4871b9e478ecddae1
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-11-02 13:41:43 +0000

    Also create output for failed suppliers
    Late-breaking requirement from CCS is to also have a file for failed suppliers for generating letters.

--- dmscripts/generate_framework_agreement_data.py
+++ dmscripts/generate_framework_agreement_data.py
@@ -39,6 +39,13 @@ class Supplier:
             self.company_number, self.registered_office_address, self.contact_name, self.contact_email)
 
 
+class FailedSupplier:
+
+    def __init__(self, declaration):
+        self.supplier_id = declaration[0]
+        self.registered_company_name = declaration[20]
+
+
 def read_csv(filepath):
     all_rows = []
     with open(filepath, 'r') as csvfile:
@@ -60,10 +67,7 @@ def make_filename_key(supplier_name, supplier_id):
 def supplier_is_on_framework(client, supplier_id):
     try:
         framework_interest = client.get_supplier_framework_info(supplier_id, 'g-cloud-7')
-        if framework_interest['frameworkInterest']['onFramework']:
-            return True
-        else:
-            return False
+        return framework_interest['frameworkInterest']['onFramework']
     except HTTPError as e:
         print("ERROR checking if supplier {} is on framework: {}".format(supplier_id, str(e)))
         return False
@@ -73,57 +77,76 @@ def build_framework_agreements(client, declarations, lots, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     with open('{}/g7-framework-data.tsv'.format(output_dir), 'w') as csvfile:
-        # This defines the order of the fields - fields can be in any order in
-        # the dictionary for each row and will be mapped to the order defined here.
-        fieldnames = [
-            'Key',
-            'Supplier ID',
-            'Registered Company Name',
-            'Country of Registration',
-            'Registered Company Number',
-            'Registered Address',
-            'Framework Contact Name',
-            'Framework Contact Email address',
-            'Lot1',
-            'Lot2',
-            'Lot3',
-            'Lot4',
-            'Lot1Letter',
-            'Lot2Letter',
-            'Lot3Letter',
-            'Lot4Letter',
-        ]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel-tab')
-        writer.writeheader()
+        with open('{}/g7-fail-data.tsv'.format(output_dir), 'w') as failfile:
+            # This defines the order of the fields - fields can be in any order in
+            # the dictionary for each row and will be mapped to the order defined here.
+            fieldnames = [
+                'Key',
+                'Supplier ID',
+                'Registered Company Name',
+                'Country of Registration',
+                'Registered Company Number',
+                'Registered Address',
+                'Framework Contact Name',
+                'Framework Contact Email address',
+                'Lot1',
+                'Lot2',
+                'Lot3',
+                'Lot4',
+                'Lot1Letter',
+                'Lot2Letter',
+                'Lot3Letter',
+                'Lot4Letter',
+            ]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel-tab')
+            writer.writeheader()
 
-        for declaration in declarations:
-            supplier_id = declaration[0]
-            if supplier_is_on_framework(client, supplier_id):
-                lot_count = lot_counts_for_supplier_id(lots, supplier_id)
-                if lot_count:
-                    supplier = Supplier(declaration, lot_count)
+            fail_fieldnames = [
+                'Key',
+                'Supplier ID',
+                'Registered Company Name'
+            ]
+            fail_writer = csv.DictWriter(failfile, fieldnames=fail_fieldnames, dialect='excel-tab')
+            fail_writer.writeheader()
+
+            for declaration in declarations:
+                supplier_id = declaration[0]
+                on_framework = supplier_is_on_framework(client, supplier_id)
+                if on_framework is True:
+                    lot_count = lot_counts_for_supplier_id(lots, supplier_id)
+                    if lot_count:
+                        supplier = Supplier(declaration, lot_count)
+                        row = {
+                            'Key': make_filename_key(supplier.registered_company_name, supplier.supplier_id),
+                            'Supplier ID': supplier.supplier_id,
+                            'Registered Company Name': supplier.registered_company_name,
+                            'Country of Registration': supplier.country_of_registration,
+                            'Registered Company Number': supplier.company_number,
+                            'Registered Address': supplier.registered_office_address,
+                            'Framework Contact Name': supplier.contact_name,
+                            'Framework Contact Email address': supplier.contact_email,
+                            'Lot1': supplier.lot1,
+                            'Lot2': supplier.lot2,
+                            'Lot3': supplier.lot3,
+                            'Lot4': supplier.lot4,
+                            'Lot1Letter': "Pass" if supplier.lot1 else "No bid",
+                            'Lot2Letter': "Pass" if supplier.lot2 else "No bid",
+                            'Lot3Letter': "Pass" if supplier.lot3 else "No bid",
+                            'Lot4Letter': "Pass" if supplier.lot4 else "No bid",
+                        }
+                        writer.writerow(row)
+                elif on_framework is False:
+                    print("Failed supplier: {}".format(supplier_id))
+                    supplier = FailedSupplier(declaration)
                     row = {
                         'Key': make_filename_key(supplier.registered_company_name, supplier.supplier_id),
                         'Supplier ID': supplier.supplier_id,
                         'Registered Company Name': supplier.registered_company_name,
-                        'Country of Registration': supplier.country_of_registration,
-                        'Registered Company Number': supplier.company_number,
-                        'Registered Address': supplier.registered_office_address,
-                        'Framework Contact Name': supplier.contact_name,
-                        'Framework Contact Email address': supplier.contact_email,
-                        'Lot1': supplier.lot1,
-                        'Lot2': supplier.lot2,
-                        'Lot3': supplier.lot3,
-                        'Lot4': supplier.lot4,
-                        'Lot1Letter': "Pass" if supplier.lot1 else "No bid",
-                        'Lot2Letter': "Pass" if supplier.lot2 else "No bid",
-                        'Lot3Letter': "Pass" if supplier.lot3 else "No bid",
-                        'Lot4Letter': "Pass" if supplier.lot4 else "No bid",
                     }
-                    writer.writerow(row)
-            else:
-                print("Skipping supplier not on framework: {}".format(supplier_id))
-                continue
+                    fail_writer.writerow(row)
+                else:
+                    print("Supplier did not apply: {}".format(supplier_id))
+                    continue
 
 
 def lot_counts_for_supplier_id(lot_counts, supplier_id):
