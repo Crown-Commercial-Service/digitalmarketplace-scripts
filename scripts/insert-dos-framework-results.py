ac5a8c8cac1b1c4dfac0322a0cb8fb41d9016af9
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-01-20 12:00:07 +0000

    Use helper function to generate API urls from <stage> param

--- scripts/insert-dos-framework-results.py
+++ scripts/insert-dos-framework-results.py
@@ -10,17 +10,17 @@ For each Digital Outcomes and Specialists application this script:
    - on_framework=NULL (no call to API) if declaration DISCRETIONARY and at least one lot not `failed`
 
 Usage:
-    scripts/insert-dos-framework-results.py <path_to_frameworks> <data_api_url> <data_api_token>
+    scripts/insert-dos-framework-results.py <path_to_frameworks> <stage> --api-token=<data_api_token>
 
 Example:
-    python scripts/insert-dos-framework-results.py ../digitalmarkeplace-frameworks http://localhost:5000 myToken
+    python scripts/insert-dos-framework-results.py ../digitalmarkeplace-frameworks dev --api-token=myToken
 """
+import sys
+sys.path.insert(0, '.')
 
 import getpass
-import sys
+from dmscripts.env import get_api_endpoint_from_stage
 from dmutils import content_loader
-
-sys.path.insert(0, '.')
 
 from docopt import docopt
 from dmapiclient import DataAPIClient
@@ -30,9 +30,9 @@ from dmutils.content_loader import ContentLoader
 if __name__ == '__main__':
     arguments = docopt(__doc__)
 
-    path_to_frameworks = arguments['<path_to_frameworks>']
-    content_loader = ContentLoader(path_to_frameworks)
-    client = DataAPIClient(arguments['<data_api_url>'], arguments['<data_api_token>'])
+    data_api_url = get_api_endpoint_from_stage(arguments['<stage>'], 'api')
+    content_loader = ContentLoader(arguments['<path_to_frameworks>'])
+    client = DataAPIClient(data_api_url, arguments['--api-token'])
     user = getpass.getuser()
 
     process_dos_results(client, content_loader, user)
