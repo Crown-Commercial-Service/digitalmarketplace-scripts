2f50cdb943ca0d457375bd2aca7e078d73f7eb37
Tom Byers <tombaromba@gmail.com>
2016-02-22 11:09:16 +0000

    Refactor to use DictReader/Writer
    We are manipulating the original CSV based on
    which columns are needed so it makes sense to use
    a `DictReader/Writer` as it allows us to refer to
    them by their field names (which are how the
    columns are identified).

--- dmscripts/generate_dos_import_data.py
+++ dmscripts/generate_dos_import_data.py
@@ -7,8 +7,15 @@ else:
     import csv
 
 
+NEW_LOT_FIELDS = ["completed_lots_{}".format(idx) for idx in range(1, 5)]
 IMPORT_DATA_CONFIG = {
     'digital-outcomes-and-specialists': {
+        'lot_fields': [
+            'completed_digital-outcomes',
+            'completed_digital-specialists',
+            'completed_user-research-studios',
+            'completed_user-research-participants',
+        ],
         'lot_names': [
             'Digital outcomes',
             'Digital specialists',
@@ -16,7 +23,7 @@ IMPORT_DATA_CONFIG = {
             'User research participants',
         ],
         'framework_agreement': {
-            'required_columns': [
+            'required_fields': [
                 'supplier_id',
                 'trading_name',
                 'registered_address',
@@ -24,26 +31,13 @@ IMPORT_DATA_CONFIG = {
             ]
         },
         'result_letter': {
-            'required_columns': [
+            'required_fields': [
                 'supplier_id',
                 'supplier_declaration_name'
             ]
         }
     },
 }
-
-
-def reduce_row_to_list(row, selected_indices):
-    result = []
-    lastIdx = len(row) - 1
-    for idx in selected_indices:
-        if idx <= lastIdx:
-            result.append(row[idx])
-    return result
-
-
-def get_required_column_indices(column_headings, required_column_headings):
-    return [column_headings.index(heading) for heading in required_column_headings]
 
 
 def fill_list_based_on_flags(list_of_flags, potential_values):
@@ -59,60 +53,41 @@ def fill_list_based_on_flags(list_of_flags, potential_values):
     return new_list
 
 
-def get_lot_results(values, lot_names):
-    # results are last 3 blocks of 4
-    number_of_lots = len(lot_names)
-    results_by_lot = {
-        'completed': values[:number_of_lots],
-        'failed': values[number_of_lots:(number_of_lots * 2)],
-        'draft': values[(number_of_lots * 2):(number_of_lots * 3)],
-    }
-    for status in results_by_lot:
-        results_by_lot[status] = fill_list_based_on_flags(results_by_lot[status], lot_names)
-    return results_by_lot
-
-
-def get_indices_of_required_columns(supplier_info_headings, required_columns):
-    return [supplier_info_headings.index(heading) for heading in required_columns]
+def get_new_field_names(required_fields):
+    field_names = required_fields
+    field_names.extend(NEW_LOT_FIELDS)
+    return field_names
 
 
 def reformat_csv_data(csv_data, framework_slug, document_type):
     config = IMPORT_DATA_CONFIG[framework_slug]
-    number_of_lots = len(config['lot_names'])
-    lot_results_start_idx = len(csv_data[1]) - (3 * number_of_lots)
-    supplier_info_headings = csv_data[0][:lot_results_start_idx]
-    required_column_indicies = get_indices_of_required_columns(
-        supplier_info_headings, config[document_type]['required_columns'])
-
+    required_fields = config[document_type]['required_fields']
     import_data = []
 
-    # start column headings with supplier info
-    import_data.append(
-        reduce_row_to_list(supplier_info_headings, required_column_indicies))
-    # add new headings for completed lots
-    import_data[0].extend(['completed_lots_{}'.format(idx) for idx in range(1, 5)])
-
     for idx, row in enumerate(csv_data):
-        if idx > 0:
-            # get all values except those for the lot results
-            supplier_info_values = row[:lot_results_start_idx]
-            import_data.append(reduce_row_to_list(supplier_info_values, required_column_indicies))
-            # add completed lots
-            lot_results = get_lot_results(
-                csv_data[idx][lot_results_start_idx:], config['lot_names'])
-            import_data[idx].extend(lot_results['completed'])
+        # get the cells for the required columns
+        new_row = {field_name: row[field_name] for field_name in required_fields}
+        # add completed lots
+        lot_results = fill_list_based_on_flags(
+            [row[lot_field] for lot_field in config['lot_fields']], config['lot_names'])
+        new_row.update(
+            {NEW_LOT_FIELDS[idx]: lot_results[idx] for idx in range(0, len(lot_results))})
+        import_data.append(new_row)
 
     return import_data
 
 
 def get_list_from_csv_file(csvfile):
-    csv_reader = csv.reader(csvfile)
+    csv_reader = csv.DictReader(csvfile)
     return [row for row in csv_reader]
 
 
-def create_import_data_file_for_documents(import_data, target_dir, document_type):
+def create_import_data_file_for_documents(import_data, target_dir, framework_slug, document_type):
+    new_field_names = get_new_field_names(
+        IMPORT_DATA_CONFIG[framework_slug][document_type]['required_fields'])
     file_path = os.path.join(target_dir, '{}_import_data.txt'.format(document_type))
-    csv_writer = csv.writer(open(file_path, 'w+'), delimiter="\t", lineterminator="\n")
+    csv_writer = csv.DictWriter(open(file_path, 'w+'), fieldnames=new_field_names, delimiter="\t", lineterminator="\n")
+    csv_writer.writeheader()
     for row in import_data:
         csv_writer.writerow(row)
 
@@ -121,4 +96,4 @@ def generate_import_data(suppliers_file_path, import_data_dir_path, framework_sl
     with open(suppliers_file_path) as suppliers_file:
         suppliers_csv_data = get_list_from_csv_file(suppliers_file)
     suppliers_data = reformat_csv_data(suppliers_csv_data, framework_slug, document_type)
-    create_import_data_file_for_documents(suppliers_data, import_data_dir_path, document_type)
+    create_import_data_file_for_documents(suppliers_data, import_data_dir_path, framework_slug, document_type)
--- tests/test_generate_dos_import_data.py
+++ tests/test_generate_dos_import_data.py
@@ -5,29 +5,25 @@ import shutil
 
 from dmscripts.generate_dos_import_data import (
     get_list_from_csv_file, fill_list_based_on_flags, reformat_csv_data,
-    get_lot_results, get_indices_of_required_columns, create_import_data_file_for_documents,
-    reduce_row_to_list)
+    create_import_data_file_for_documents, get_new_field_names)
 
 
 CSV_FILE_LIST = [
-    ["supplier_name", "supplier_declaration_name", "supplier_id", "trading_name", "registered_address", "company_number", "country_of_registration", "contact_name", "contact_email", "completed_digital-outcomes", "completed_digital-specialists", "completed_user-research-studios", "completed_user-research-participants", "failed_digital-outcomes", "failed_digital-specialists", "failed_user-research-studios", "failed_user-research-participants", "draft_digital-outcomes", "draft_digital-specialists", "draft_user-research-studios", "draft_user-research-participants"],  # noqa
-    ["CloudTech Limited", "CloudTech limited", "12345", "CloudTech Limited", "21 Fenchurch Street, London, EC3M 3BY", "123456789", "United Kingdom", "Brian Church", "brian.church@cloudtech.com", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],  # noqa
-    ["Human Research Systems Limited", "Human Research Systems limited", "67890", "Human Research Systems Limited", "45 Clerkenwell Road, London, EC1R 5BL", "987654321", "United Kingdom", "Kevin Briggs", "kevin.briggs@hrs.com", "0", "0", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0"],  # noqa
-    ["Total Web Limited", "Total Web limited", "66789", "Total Web Limited", "33 Fore Street, London, EC2Y 5EJ", "678901234", "United Kingdom", "Sue Watson", "sue.watch@totalweb.com", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1"]  # noqa
+    {"supplier_name": "CloudTech Limited", "supplier_declaration_name": "CloudTech limited", "supplier_id": "12345", "trading_name": "CloudTech Limited", "registered_address": "21 Fenchurch Street, London, EC3M 3BY", "company_number": "123456789", "country_of_registration": "United Kingdom", "contact_name": "Brian Church", "contact_email": "brian.church@cloudtech.com", "completed_digital-outcomes": "1", "completed_digital-specialists": "1", "completed_user-research-studios": "0", "completed_user-research-participants": "0", "failed_digital-outcomes": "0", "failed_digital-specialists": "0", "failed_user-research-studios": "0", "failed_user-research-participants": "0", "draft_digital-outcomes": "0", "draft_digital-specialists": "0", "draft_user-research-studios": "0", "draft_user-research-participants": "0"},  # noqa
+    {"supplier_name": "Human Research Systems Limited", "supplier_declaration_name": "Human Research Systems limited", "supplier_id": "67890", "trading_name": "Human Research Systems Limited", "registered_address": "45 Clerkenwell Road, London, EC1R 5BL", "company_number": "987654321", "country_of_registration": "United Kingdom", "contact_name": "Kevin Briggs", "contact_email": "kevin.briggs@hrs.com", "completed_digital-outcomes": "0", "completed_digital-specialists": "0", "completed_user-research-studios": "1", "completed_user-research-participants": "1", "failed_digital-outcomes": "0", "failed_digital-specialists": "0", "failed_user-research-studios": "0", "failed_user-research-participants": "0", "draft_digital-outcomes": "0", "draft_digital-specialists": "0", "draft_user-research-studios": "0", "draft_user-research-participants": "0"},  # noqa
+    {"supplier_name": "Total Web Limited", "supplier_declaration_name": "Total Web limited", "supplier_id": "66789", "trading_name": "Total Web Limited", "registered_address": "33 Fore Street, London, EC2Y 5EJ", "company_number": "678901234", "country_of_registration": "United Kingdom", "contact_name": "Sue Watson", "contact_email": "sue.watch@totalweb.com", "completed_digital-outcomes": "1", "completed_digital-specialists": "1", "completed_user-research-studios": "0", "completed_user-research-participants": "0", "failed_digital-outcomes": "0", "failed_digital-specialists": "0", "failed_user-research-studios": "0", "failed_user-research-participants": "0", "draft_digital-outcomes": "0", "draft_digital-specialists": "0", "draft_user-research-studios": "1", "draft_user-research-participants": "1"}  # noqa
 ]
 
 FRAMEWORK_AGREEMENT_IMPORT_DATA_LIST = [
-    ["trading_name", "registered_address", "company_number", "completed_lots_1", "completed_lots_2", "completed_lots_3", "completed_lots_4"],  # noqa
-    ["CloudTech Limited", "21 Fenchurch Street, London, EC3M 3BY", "123456789", "Digital outcomes", "Digital specialists", "", ""],  # noqa
-    ["Human Research Systems Limited", "45 Clerkenwell Road, London, EC1R 5BL", "987654321", "User research studios", "User research participants", "", ""],  # noqa
-    ["Total Web Limited", "33 Fore Street, London, EC2Y 5EJ", "678901234", "Digital outcomes", "Digital specialists", "", ""]  # noqa
+    {"supplier_id": "12345", "trading_name": "CloudTech Limited", "registered_address": "21 Fenchurch Street, London, EC3M 3BY", "company_number": "123456789", "completed_lots_1": "Digital outcomes", "completed_lots_2": "Digital specialists", "completed_lots_3": "", "completed_lots_4": ""},  # noqa
+    {"supplier_id": "67890", "trading_name": "Human Research Systems Limited", "registered_address": "45 Clerkenwell Road, London, EC1R 5BL", "company_number": "987654321", "completed_lots_1": "User research studios", "completed_lots_2": "User research participants", "completed_lots_3": "", "completed_lots_4": ""},  # noqa
+    {"supplier_id": "66789", "trading_name": "Total Web Limited", "registered_address": "33 Fore Street, London, EC2Y 5EJ", "company_number": "678901234", "completed_lots_1": "Digital outcomes", "completed_lots_2": "Digital specialists", "completed_lots_3": "", "completed_lots_4": ""}  # noqa
 ]
 
 RESULT_LETTER_IMPORT_DATA_LIST = [
-    ["supplier_id", "supplier_declaration_name", "completed_lots_1", "completed_lots_2", "completed_lots_3", "completed_lots_4"],  # noqa
-    ["12345", "CloudTech limited",  "Digital outcomes", "Digital specialists", "", ""],  # noqa
-    ["67890", "Human Research Systems limited", "User research studios", "User research participants", "", ""],  # noqa
-    ["66789", "Total Web limited", "Digital outcomes", "Digital specialists", "", ""]  # noqa
+    {"supplier_id": "12345", "supplier_declaration_name": "CloudTech limited", "completed_lots_1": "Digital outcomes", "completed_lots_2": "Digital specialists", "completed_lots_3": "", "completed_lots_4": ""},  # noqa
+    {"supplier_id": "67890", "supplier_declaration_name": "Human Research Systems limited", "completed_lots_1": "User research studios", "completed_lots_2": "User research participants", "completed_lots_3": "", "completed_lots_4": ""},  # noqa
+    {"supplier_id": "66789", "supplier_declaration_name": "Total Web limited", "completed_lots_1": "Digital outcomes", "completed_lots_2": "Digital specialists", "completed_lots_3": "", "completed_lots_4": ""}  # noqa
 ]
 
 
@@ -56,85 +52,18 @@ def test_fill_list_based_on_flags_for_varied_sequence():
     ) == ["digital-outcomes", "user-research-studios", "", ""]
 
 
-def test_reduce_row_to_list_with_linear_sequence():
-    assert reduce_row_to_list([
-        "CloudTech Limited", "CloudTech limited", "12345",
-        "CloudTech Limited", "21 Fenchurch Street, London, EC3M 3BY"
-    ], [0, 1, 2, 3]) == [
-        "CloudTech Limited", "CloudTech limited", "12345",
-        "CloudTech Limited"
+def test_get_new_field_names():
+    assert get_new_field_names([
+        'supplier_id',
+        'trading_name',
+    ]) == [
+        'supplier_id',
+        'trading_name',
+        'completed_lots_1',
+        'completed_lots_2',
+        'completed_lots_3',
+        'completed_lots_4',
     ]
-
-
-def test_reduce_row_to_list_with_varied_sequence():
-    assert reduce_row_to_list([
-        "CloudTech Limited", "CloudTech limited", "12345",
-        "CloudTech Limited", "21 Fenchurch Street, London, EC3M 3BY"
-    ], [0, 2, 4]) == [
-        "CloudTech Limited", "12345",
-        "21 Fenchurch Street, London, EC3M 3BY"
-    ]
-
-
-def test_get_lot_results_for_supplier_with_only_completed_lots():
-    assert get_lot_results(
-        CSV_FILE_LIST[1][len(CSV_FILE_LIST[1]) - 12:],
-        [
-            "Digital outcomes",
-            "Digital specialists",
-            "User research studios",
-            "User research participants",
-        ]) == {
-        "completed": [
-            "Digital outcomes",
-            "Digital specialists",
-            "",
-            ""
-        ],
-        "failed": ["", "", "", ""],
-        "draft": ["", "", "", ""]
-    }
-
-
-def test_get_lot_results_for_supplier_with_different_lots():
-    assert get_lot_results(
-        CSV_FILE_LIST[3][len(CSV_FILE_LIST[3]) - 12:],
-        [
-            "Digital outcomes",
-            "Digital specialists",
-            "User research studios",
-            "User research participants",
-        ]) == {
-        "completed": [
-            "Digital outcomes",
-            "Digital specialists",
-            "",
-            ""
-        ],
-        "failed": ["", "", "", ""],
-        "draft": [
-            "User research studios",
-            "User research participants",
-            "",
-            ""
-        ]
-    }
-
-
-def test_get_indices_of_required_columns():
-    assert get_indices_of_required_columns(
-        CSV_FILE_LIST[0],
-        [
-            "supplier_name", "supplier_id", "company_number",
-        ]
-    ) == [0, 2, 5]
-
-
-def test_get_indices_of_required_columns_if_non_required():
-    assert get_indices_of_required_columns(
-        CSV_FILE_LIST[0],
-        []
-    ) == []
 
 
 def test_reformat_csv_data_for_framework_agreement():
@@ -154,20 +83,20 @@ def test_create_import_data_file_for_supplier_for_framework_agreement():
     create_import_data_file_for_documents(
         FRAMEWORK_AGREEMENT_IMPORT_DATA_LIST,
         dir_path,
+        'digital-outcomes-and-specialists',
         'framework_agreement'
     )
-    example_import_data_file = open('tests/fixtures/example-dos-framework-agreement-import-data.txt')
-    created_file_path = os.path.join(dir_path, 'framework_agreement_import_data.txt')
-    assert os.path.isfile(created_file_path)
-    created_file = open(created_file_path)
-    example_import_data_file_contents = example_import_data_file.read()
-    created_file_contents = created_file.read()
+    with open('tests/fixtures/example-dos-framework-agreement-import-data.txt') as example_import_data_file:  # noqa
+        created_file_path = os.path.join(dir_path, 'framework_agreement_import_data.txt')
+        try:
+            assert os.path.isfile(created_file_path)
+            with open(created_file_path) as created_file:
+                example_import_data_file_contents = example_import_data_file.read()
+                created_file_contents = created_file.read()
 
-    created_file.close()
-    example_import_data_file.close()
-    shutil.rmtree(dir_path)
-
-    assert example_import_data_file_contents == created_file_contents
+                assert example_import_data_file_contents == created_file_contents
+        finally:
+            shutil.rmtree(dir_path)
 
 
 def test_create_import_data_file_for_supplier_for_result_letter():
@@ -175,17 +104,17 @@ def test_create_import_data_file_for_supplier_for_result_letter():
     create_import_data_file_for_documents(
         RESULT_LETTER_IMPORT_DATA_LIST,
         dir_path,
+        'digital-outcomes-and-specialists',
         'result_letter'
     )
-    example_import_data_file = open('tests/fixtures/example-dos-result-letter-import-data.txt')
-    created_file_path = os.path.join(dir_path, 'result_letter_import_data.txt')
-    assert os.path.isfile(created_file_path)
-    created_file = open(created_file_path)
-    example_import_data_file_contents = example_import_data_file.read()
-    created_file_contents = created_file.read()
+    with open('tests/fixtures/example-dos-result-letter-import-data.txt') as example_import_data_file:  # noqa
+        created_file_path = os.path.join(dir_path, 'result_letter_import_data.txt')
+        try:
+            assert os.path.isfile(created_file_path)
+            with open(created_file_path) as created_file:
+                example_import_data_file_contents = example_import_data_file.read()
+                created_file_contents = created_file.read()
 
-    created_file.close()
-    example_import_data_file.close()
-    shutil.rmtree(dir_path)
-
-    assert example_import_data_file_contents == created_file_contents
+                assert example_import_data_file_contents == created_file_contents
+        finally:
+            shutil.rmtree(dir_path)
