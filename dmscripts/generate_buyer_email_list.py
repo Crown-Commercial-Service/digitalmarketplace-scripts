32d471c3297d38fd8a178b503810762b3933a7f8
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-05-05 17:15:10 +0100

    Add script to export buyer users and their briefs

---
+++ dmscripts/generate_buyer_email_list.py
@@ -0,0 +1,59 @@
+from multiprocessing.pool import ThreadPool
+from dmapiclient import HTTPError
+from dmscripts.logging import configure_logger
+import sys
+if sys.version_info > (3, 0):
+    import csv
+else:
+    import unicodecsv as csv
+
+
+def buyer_requirements(client):
+    def inner_function(user):
+        try:
+            briefs = client.find_briefs(user_id=user['id'])['briefs']
+            if briefs:
+                title_statuses = ["; ".join(["{} - {}".format(brief['title'], brief['status']) for brief in briefs])]
+            else:
+                title_statuses = []
+        except HTTPError as e:
+            if e.status_code == 404:
+                title_statuses = []
+            else:
+                print("HTTP ERROR: {}".format(e))
+        except Exception as e:
+            print("EXCEPTION: {}".format(e))
+
+        return user, title_statuses
+
+    return inner_function
+
+
+def add_buyer_requirements(client, users):
+    pool = ThreadPool(10)
+    callback = buyer_requirements(client)
+    return pool.imap_unordered(callback, users)
+
+
+def find_buyer_users(client):
+    for user in client.find_users_iter():
+        if user['active'] and user['role'] == 'buyer':
+            yield user
+
+
+def list_buyers(client, output, include_briefs):
+    writer = csv.writer(output, delimiter=',', quotechar='"')
+    users = find_buyer_users(client)
+
+    if include_briefs:
+        users = add_buyer_requirements(client, users)
+    else:
+        users = ((user, None) for user in users)
+
+    for user, briefs in users:
+        row = [
+            user['name'],
+            user['emailAddress'],
+        ]
+        row += briefs if briefs else []
+        writer.writerow(row)
