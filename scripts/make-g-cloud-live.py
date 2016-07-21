3614fa4473498e5cecea4a4ea30e53e6b7e12b66
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-07-21 15:04:39 +0100

    Allow script to cope with Unicode in supplier names

--- scripts/make-g-cloud-live.py
+++ scripts/make-g-cloud-live.py
@@ -27,14 +27,14 @@ DOCUMENT_KEYS = [
 
 
 def assert_equal(one, two):
-    assert one == two, "{} != {}".format(one, two)
+    assert one == two, u"{} != {}".format(one, two)
 
 
 def parse_document_url(url, framework_slug):
     pattern = r'/{}/submissions/(\d+)/(\d+)-(.*)$'.format(re.escape(framework_slug))
     match = re.search(pattern, url)
     if not match:
-        raise ValueError("Could not parse document URL {}".format(url))
+        raise ValueError(u"Could not parse document URL {}".format(url))
     return {
         'supplier_id': match.group(1),
         'draft_id': match.group(2),
@@ -43,57 +43,57 @@ def parse_document_url(url, framework_slug):
 
 
 def get_draft_document_path(parsed_document, framework_slug):
-    return '{framework_slug}/submissions/{supplier_id}/{draft_id}-{document_name}'.format(
+    return u'{framework_slug}/submissions/{supplier_id}/{draft_id}-{document_name}'.format(
         framework_slug=framework_slug,
         **parsed_document)
 
 
 def get_live_document_path(parsed_document, framework_slug, service_id):
-    return '{framework_slug}/documents/{supplier_id}/{service_id}-{document_name}'.format(
+    return u'{framework_slug}/documents/{supplier_id}/{service_id}-{document_name}'.format(
         framework_slug=framework_slug,
         service_id=service_id,
         **parsed_document)
 
 
 def get_live_asset_url(live_document_path):
-    return "{}/{}".format(get_assets_endpoint_from_stage(STAGE), live_document_path)
+    return u"{}/{}".format(get_assets_endpoint_from_stage(STAGE), live_document_path)
 
 
 def document_copier(draft_bucket, documents_bucket, dry_run):
     def copy_document(draft_document_path, live_document_path):
         if not draft_bucket.path_exists(draft_document_path):
-            raise ValueError("Draft document {} does not exist in bucket {}".format(
+            raise ValueError(u"Draft document {} does not exist in bucket {}".format(
                 draft_document_path, draft_bucket.bucket_name))
-        message_suffix = "{}:{} to {}:{}".format(
+        message_suffix = u"{}:{} to {}:{}".format(
             draft_bucket.bucket_name, draft_document_path,
             documents_bucket.bucket_name, live_document_path)
 
         if dry_run:
-            print("    > not copying {}".format(message_suffix))
+            print(u"    > not copying {}".format(message_suffix))
         else:
             key = documents_bucket.bucket.copy_key(live_document_path,
                                                    src_bucket_name=draft_bucket.bucket_name,
                                                    src_key_name=draft_document_path)
             key.set_acl('public-read')
-            print("    > copied {}".format(message_suffix))
+            print(u"    > copied {}".format(message_suffix))
 
     return copy_document
 
 
 def make_draft_service_live(client, copy_document, draft_service, framework_slug, dry_run):
-    print("  > Migrating draft {} - {}".format(draft_service['id'], draft_service['serviceName']))
+    print(u"  > Migrating draft {} - {}".format(draft_service['id'], draft_service['serviceName']))
     if dry_run:
         service_id = random.randint(1000, 10000)
-        print("    > generating random test service ID: {}".format(service_id))
+        print(u"    > generating random test service ID: {}".format(service_id))
     else:
         services = client.publish_draft_service(draft_service['id'], 'make-g-cloud-live script')
         service_id = services['services']['id']
-        print("    > draft service published - new service ID {}".format(service_id))
+        print(u"    > draft service published - new service ID {}".format(service_id))
 
     document_updates = {}
     for document_key in DOCUMENT_KEYS:
         if document_key not in draft_service:
-            print("    > Skipping {}".format(document_key))
+            print(u"    > Skipping {}".format(document_key))
         else:
             parsed_document = parse_document_url(draft_service[document_key], framework_slug)
             assert_equal(str(parsed_document['supplier_id']), str(draft_service['supplierId']))
@@ -106,7 +106,7 @@ def make_draft_service_live(client, copy_document, draft_service, framework_slug
             document_updates[document_key] = get_live_asset_url(live_document_path)
 
     if dry_run:
-        print("    > not updating document URLs {}".format(document_updates))
+        print(u"    > not updating document URLs {}".format(document_updates))
     else:
         client.update_service(service_id, document_updates, 'Moving documents to live bucket')
         print("    > document URLs updated")
@@ -128,7 +128,7 @@ if __name__ == '__main__':
     suppliers = find_suppliers_on_framework(client, FRAMEWORK_SLUG)
 
     for supplier in suppliers:
-        print("Migrating drafts for supplier {} - {}".format(supplier['supplierId'], supplier['supplierName']))
+        print(u"Migrating drafts for supplier {} - {}".format(supplier['supplierId'], supplier['supplierName']))
         draft_services = get_submitted_drafts(client, FRAMEWORK_SLUG, supplier['supplierId'])
 
         for draft_service in draft_services:
