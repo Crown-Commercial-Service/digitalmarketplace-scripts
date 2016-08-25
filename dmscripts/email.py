bfea9994d0e2ff02cddcc6579005fd3f05d0e0c1
Alexey Bezhan <al@fueledbylemons.com>
2016-08-25 17:15:45 +0100

    Add a function to search sent Mandrill messages
    Requests tagged messages from Mandrill history. Mandrill stores
    sent messages for 7 days and filtered by tag we don't expect to
    send more than 1000 messages so we can avoid adding pagination
    for now.
    
    Note that messages don't appear in Mandrill search API response
    until they've been delivered, so rerunning the send script right
    away will probably end up sending duplicate emails.

--- dmscripts/email.py
+++ dmscripts/email.py
@@ -9,3 +9,9 @@ def render_html(template_path, data=None):
         html = htmlfile.read()
         template = Template(html)
         return template.render(data or {})
+
+
+def get_sent_emails(mandrill_api_key, tags, date_from=None):
+    mandrill_client = Mandrill(mandrill_api_key)
+
+    return mandrill_client.messages.search(tags=tags, date_from=date_from, limit=1000)
