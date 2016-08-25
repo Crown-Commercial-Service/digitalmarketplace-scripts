f6560edaf096b159eebfcdea0213523453c8a6db
Alexey Bezhan <al@fueledbylemons.com>
2016-08-25 17:25:03 +0100

    [#125516661] Add a script to notify buyers when requirement closes
    Gets the list of briefs that closed on a given date (yesterday by
    default) from the API, checks brief IDs against Mandrill history
    of sent 'requirements-closed' emails and sends notification to brief
    users for briefs that don't have an email history in Mandrill.
    
    Since briefs aren't ordered by close date we need to download all
    closed briefs from the API. There are only few pages at the moment,
    so this shouldn't be a problem for now.
    
    Sent emails don't appear in mandrill history until the message has
    been delivered, so rerunning the script right away could result in
    duplicate emails.
    
    Doesn't allow to send notifications for briefs that closed more
    than a week ago. We probably don't need to notify users about
    something that happened a long time ago and this helps to avoid
    typos.
    
    Yesterday is determined from UTC time, so running the script at
    midnight UTC+1 could be a bit confusing.

---
+++ dmscripts/notify_buyers_when_requirements_close.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+
+import dmapiclient
+from dmutils.formats import DATE_FORMAT, DATETIME_FORMAT
+from dmutils.email import send_email, MandrillException
+
+from .email import render_html, get_sent_emails
+
+from . import logging
+
+logger = logging.configure_logger({'dmapiclient': logging.INFO})
+
+
+def get_closed_briefs(data_api_client, closed_at):
+    return [
+        brief for brief in data_api_client.find_briefs_iter(status='closed', with_users=True)
+        if datetime.strptime(brief['applicationsClosedAt'], DATETIME_FORMAT).date() == closed_at
+    ]
+
+
+def notify_users(email_api_key, brief):
+    logger.info("Notifying users about brief ID: {brief_id} - '{brief_title}'",
+                extra={'brief_title': brief['title'], 'brief_id': brief['id']})
+    for user in brief['users']:
+        try:
+            email_body = render_html('email_templates/requirements_closed.html', data={
+                'brief_id': brief['id'],
+                'brief_title': brief['title'],
+                'lot_slug': brief['lotSlug'],
+                'framework_slug': brief['frameworkSlug']
+            })
+            send_email(
+                [user['emailAddress'] for user in brief['users'] if user['active']],
+                email_body,
+                email_api_key,
+                u'Next steps for your ‘{}’ requirements'.format(brief['title']),
+                'enquiries@digitalmarketplace.service.gov.uk',
+                'Digital Marketplace Admin',
+                ['requirements-closed'],
+                metadata={'brief_id': brief['id']},
+                logger=logger
+            )
+
+            return True
+        except MandrillException as e:
+            logger.error(
+                "Email failed to send for brief_id: {brief_id}",
+                extra={'error': e, 'brief_id': brief['id']}
+            )
+
+            return False
+
+
+def get_closed_at(closed_at):
+    if closed_at is None:
+        return (datetime.utcnow() - timedelta(days=1)).date()
+    else:
+        return datetime.strptime(closed_at, DATE_FORMAT).date()
+
+
+def get_notified_briefs(email_api_key, closed_at):
+    return set(
+        email['metadata']['brief_id']
+        for email in get_sent_emails(email_api_key, ['requirements-closed'], date_from=closed_at.isoformat())
+        if 'brief_id' in (email.get('metadata') or {})
+    )
+
+
+def main(data_api_url, data_api_access_token, email_api_key, closed_at, dry_run):
+    logger.info("Data API URL: {data_api_url}", extra={'data_api_url': data_api_url})
+
+    closed_at = get_closed_at(closed_at)
+    if closed_at < (datetime.utcnow() - timedelta(days=8)).date():
+        logger.error('Not allowed to notify about briefs that closed more than a week ago')
+        return False
+
+    data_api_client = dmapiclient.DataAPIClient(data_api_url, data_api_access_token)
+
+    closed_briefs = get_closed_briefs(data_api_client, closed_at)
+    if not closed_briefs:
+        logger.info("No briefs closed on {closed_at}", extra={"closed_at": closed_at})
+        return True
+
+    logger.info("Notifying users about {briefs_count} closed briefs", extra={'briefs_count': len(closed_briefs)})
+
+    notified_briefs = get_notified_briefs(email_api_key, closed_at)
+
+    logger.info('Brief notifications sent since {closed_at}: {notified_briefs_count}',
+                extra={'closed_at': closed_at, 'notified_briefs_count': len(notified_briefs)})
+
+    for brief in closed_briefs:
+        if brief['id'] in notified_briefs:
+            logger.info('Brief notification already sent for brief ID: {brief_id}', extra={'brief_id': brief['id']})
+        elif dry_run:
+            logger.info("Would notify users about brief ID: {brief_id}", extra={'brief_id': brief['id']})
+        else:
+            status = notify_users(email_api_key, brief)
+            if not status:
+                return False
+
+    return True
---
+++ scripts/notify-buyers-when-requirements-close.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""Send email notifications to buyer users about closed requirements.
+
+Usage:
+    notify-buyers-when-requirements-close.py <stage> --api-token=<api_access_token>
+                                                     --email-api-key=<email_api_key> [options]
+
+    --closed-at=<date>  Notify about requirements that closed on the given date (date format: YYYY-MM-DD)
+    --dry-run  List notifications that would be sent without sending the emails
+
+"""
+
+import sys
+
+from docopt import docopt
+
+sys.path.insert(0, '.')
+from dmscripts.env import get_api_endpoint_from_stage
+from dmscripts.notify_buyers_when_requirements_close import main
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
+    ok = main(
+        data_api_url=get_api_endpoint_from_stage(arguments['<stage>'], 'api'),
+        data_api_access_token=arguments['--api-token'],
+        email_api_key=arguments['--email-api-key'],
+        closed_at=arguments['--closed-at'],
+        dry_run=arguments['--dry-run']
+    )
+
+    if not ok:
+        sys.exit(1)
