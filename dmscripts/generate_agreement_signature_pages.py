800c277ee12b53c8628b9f978eb482ecf8076c3c
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-11-15 17:25:45 +0000

    Add script to generate counterpart G-Cloud 8 PDFs
    I started off parameterising the `generate-agreement-sinature-pages` script, but it turns out that there are enough differences with generating the counterpart pages that it makes more sense to me as a separate (but very similar) thing.
    
    The main differences are different conditions on when a file should be generated, an extra file that needs to be copied and later removed, (i.e. the signature file), and file names.
    
    It could probably have been parameterised and just one script, but would just involve an explosion of command line arguments that wouldn't be very pretty.

--- dmscripts/generate_agreement_signature_pages.py
+++ dmscripts/generate_agreement_signature_pages.py
@@ -27,6 +27,26 @@ def render_html_for_successful_suppliers(rows, framework, template_dir, output_d
     shutil.copyfile(template_css_path, os.path.join(output_dir, '{}-signature-page.css'.format(framework)))
 
 
+def render_html_for_suppliers_awaiting_countersignature(rows, framework, template_dir, output_dir):
+    template_path = os.path.join(template_dir, '{}-counterpart-signature-page.html'.format(framework))
+    template_css_path = os.path.join(template_dir, '{}-signature-page.css'.format(framework))
+    countersignature_img_path = os.path.join(template_dir, '{}-countersignature.png'.format(framework))
+    for data in rows:
+        if data['on_framework'] is False or not data['countersigned_at'] or data['countersigned_path']:
+            print("SKIPPING {}: on_f={} at={} path={}".format(
+                data['supplier_id'],
+                data['on_framework'],
+                data['countersigned_at'],
+                data['countersigned_path'])
+            )
+            continue
+        data['appliedLots'] = filter(lambda lot: int(data[lot]) > 0, ['saas', 'paas', 'iaas', 'scs'])
+        html = render_html(template_path, data)
+        save_page(html, data['supplier_id'], output_dir)
+    shutil.copyfile(template_css_path, os.path.join(output_dir, '{}-signature-page.css'.format(framework)))
+    shutil.copyfile(countersignature_img_path, os.path.join(output_dir, '{}-countersignature.png'.format(framework)))
+
+
 def render_pdf_for_each_html_page(html_pages, html_dir, pdf_dir):
     html_dir = os.path.abspath(html_dir)
     pdf_dir = os.path.abspath(pdf_dir)
