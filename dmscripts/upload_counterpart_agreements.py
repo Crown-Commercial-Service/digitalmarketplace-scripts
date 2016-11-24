c8f467717654b9f4f64b80d856376d6540f9ef1a
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-11-24 15:04:25 +0000

    Add script to upload countersigned agreement files
    This will upload a folder of counterpart pdf files to S3 and mark the countersignedAgreementPath in the API for the relevant FrameworkAgreement.
    
    Files are uploaded to S3 with a unique (timestamped) filename, and the Content-Disposition header is set to a more user-friendly filename that includes the supplier name and without a timestamp.

---
+++ dmscripts/upload_counterpart_agreements.py
@@ -0,0 +1,47 @@
+import getpass
+
+from dmutils.documents import generate_timestamped_document_upload_path
+
+from dmscripts.bulk_upload_documents import get_supplier_id_from_framework_file_path, generate_download_filename
+
+COUNTERPART_FILENAME = "agreement-countersignature.pdf"
+
+
+def upload_counterpart_file(bucket, framework_slug, file_path, dry_run, client, logger):
+    supplier_id = get_supplier_id_from_framework_file_path(file_path)
+    supplier_framework = client.get_supplier_framework_info(supplier_id, framework_slug)
+    supplier_framework = supplier_framework['frameworkInterest']
+    supplier_name = supplier_framework['declaration']['nameOfOrganisation']
+    download_filename = generate_download_filename(supplier_id, COUNTERPART_FILENAME, supplier_name)
+
+    upload_path = generate_timestamped_document_upload_path(
+        framework_slug,
+        supplier_id,
+        "agreements",
+        COUNTERPART_FILENAME
+    )
+    try:
+        if not dry_run:
+            # Upload file
+            with open(file_path) as file_contents:
+                bucket.save(upload_path, file_contents, acl='private', move_prefix=None,
+                            download_filename=download_filename)
+                logger.info("UPLOADED: '{}' to '{}'".format(file_path, upload_path))
+
+            # Save filepath to framework agreement
+            client.update_framework_agreement(
+                supplier_framework['agreementId'],
+                {"countersignedAgreementPath": upload_path},
+                'upload-counterpart-agreements script run by {}'.format(getpass.getuser())
+            )
+            logger.info("countersignedAgreementPath='{}' for agreement ID {}".format(
+                upload_path, supplier_framework['agreementId'])
+            )
+        else:
+            logger.info("[Dry-run] UPLOAD: '{}' to '{}'".format(file_path, upload_path))
+            logger.info("[Dry-run] countersignedAgreementPath='{}' for agreement ID {}".format(
+                upload_path, supplier_framework['agreementId'])
+            )
+
+    except Exception as e:
+        logger.info("ERROR {}".format(e.message))
