c948d29a1ed928d3f964d39c97e8cbd7110bb440
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-11-24 15:04:25 +0000

    Add script to upload countersigned agreement files
    This will upload a folder of counterpart pdf files to S3 and mark the countersignedAgreementPath in the API for the relevant FrameworkAgreement.
    
    Files are uploaded to S3 with a unique (timestamped) filename, and the Content-Disposition header is set to a more user-friendly filename that includes the supplier name and without a timestamp.

---
+++ scripts/upload-counterpart-agreements.py
@@ -0,0 +1,60 @@
+"""
+This will:
+ * scan a directory for pdf files (they should be in the format <supplier_id>-some-ignored-words.pdf but this isn't
+   enforced)
+   and for each file:
+
+   * get the SupplierFramework object for the supplier
+
+   * upload them to the S3 documents bucket for <stage> with the file path:
+     <framework_slug>/agreements/<supplier_id>/<supplier_id>-counterpart-signature-page.pdf
+     e.g.
+     g-cloud-8/agreements/1234/1234-counterpart-signature-page.pdf
+
+   * set a "download filename" that the file will be downloaded as, which is:
+     <supplier-name>-<supplier-id>-<document-name>.<document-type>
+     where supplier_name is the sanitised version of the name from their supplier declaration for the framework
+
+   * set the "countersigned agreement path" in the current FrameworkAgreement referenced by the SupplierFramework
+
+Usage:
+   scripts/upload-counterpart-agreemets.py <stage> <api_token> <documents_directory> <framework_slug> [--dry-run]
+
+"""
+
+import sys
+sys.path.insert(0, '.')
+
+from dmscripts import logging
+from dmscripts.env import get_api_endpoint_from_stage
+from dmscripts.bulk_upload_documents import get_bucket_name, get_all_files_of_type
+from dmscripts.upload_counterpart_agreements import upload_counterpart_file
+from docopt import docopt
+from dmapiclient import DataAPIClient
+
+from dmutils.s3 import S3
+
+
+logger = logging.configure_logger({'dmapiclient.base': logging.WARNING})
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    stage = arguments['<stage>']
+    client = DataAPIClient(get_api_endpoint_from_stage(stage), arguments['<api_token>'])
+    framework_slug = arguments['<framework_slug>']
+    document_directory = arguments['<documents_directory>']
+    dry_run = arguments['--dry-run']
+
+    if dry_run:
+        bucket = None
+    else:
+        bucket = S3(get_bucket_name(stage, "agreements"))
+
+    for file_path in get_all_files_of_type(document_directory, "pdf"):
+        try:
+            upload_counterpart_file(bucket, framework_slug, file_path, dry_run, client, logger)
+        except Exception as e:
+            logger.info("COULD NOT UPLOAD {}: {}".format(file_path, e.message))
+            continue
