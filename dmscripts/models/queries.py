e9de2639ac332675199492a27ac6eea011e29872
George Lund <george.lund@digital.cabinet-office.gov.uk>
2016-12-06 13:54:10 +0000

    Fix counting, pivot counts into two columns
     - nearly working [#132760553]
     - agg(np.size) was adding up accross columns, not what we wanted!
     - pivot successful and unsuccessful counts into separate columns

--- dmscripts/models/queries.py
+++ dmscripts/models/queries.py
@@ -12,6 +12,12 @@ def successful_supplier_applications(brief_responses, briefs):
 def briefs_with_summary_application_data(brief_responses, briefs):
     result = briefs.merge(brief_responses, left_on='id', right_on='briefId')
     group_cols = ['briefId', 'lot', 'title', 'status', 'essentialRequirements']
-    counted = result[group_cols].groupby(group_cols).agg(np.size).reset_index()
-    counted.columns.values[-1] = 'number of applications'
-    return counted
+    counted = result.groupby(group_cols).size().reset_index()
+    counted.columns.values[-1] = 'appcount'
+    non_pivot_cols = group_cols[:-1]
+    pivoted = counted.pivot_table(values='appcount', index=non_pivot_cols,
+                                  columns='essentialRequirements').reset_index()
+    pivoted.columns.values[-1] = 'successful applications'
+    pivoted.columns.values[-2] = 'failed applications'
+
+    return pivoted
