8299cd85513c2fe5295017a9134a96c92336566e
benvand <benjaminfvandersteen@gmail.com>
2016-12-12 15:15:01 +0000

    test updates for last commit

--- tests/test_generate_framework_master_csv.py
+++ tests/test_generate_framework_master_csv.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Tests for GenerateMasterCSV class."""
+from collections import OrderedDict
 import json
 
 try:
@@ -16,18 +17,17 @@ def test_get_fieldnames(mock_data_client):
     csv_builder = GenerateMasterCSV(client=mock_data_client, target_framework_slug='test_framework_slug')
     assert mock_data_client.get_framework.called_once_with('test_framework_slug')
 
-    csv_builder.lot_name_prefixes = ['test_lot_prefix_1', 'test_lot_prefix_2']
-    csv_builder.static_fieldnames = ['test_static_fieldname_1', 'test_static_fieldname_2']
+    csv_builder.service_status_labels = OrderedDict([('key1', 'test_lot_prefix_1'), ('key2', 'test_lot_prefix_2')])
+    csv_builder.static_fieldnames = ('test_static_fieldname_1', 'test_static_fieldname_2')
 
-    expected_data = [
+    expected_data = tuple([
         'test_static_fieldname_1',
         'test_static_fieldname_2',
         'test_lot_prefix_1_test_lot_slug_1',
         'test_lot_prefix_2_test_lot_slug_1',
         'test_lot_prefix_1_test_lot_slug_2',
         'test_lot_prefix_2_test_lot_slug_2'
-    ]
-
+    ])
     assert csv_builder.get_fieldnames() == expected_data
 
 
@@ -75,7 +75,7 @@ def test_one_supplier_one_lot(mock_data_client):
         ]
     }
     mock_data_client.find_draft_services_iter.return_value = iter([{
-        'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'
+        'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'
     }])
 
     csv_builder = GenerateMasterCSV(client=mock_data_client, target_framework_slug='test_framework_slug')
@@ -90,9 +90,9 @@ def test_one_supplier_one_lot(mock_data_client):
 def test_many_suppliers_many_lots(mock_data_client):
     """Full test for creating the csv using realistic data."""
     lot_dicts = [
-        {'slug': 'saas', 'extraneous_field': 'foo'},
-        {'slug': 'test_lot', 'extraneous_field': 'foo'},
-        {'slug': 'test_lot2', 'extraneous_field': 'foo'},
+        {'slug': 'saas', 'lotSlug': 'saas', 'extraneous_field': 'foo'},
+        {'slug': 'test_lot', 'lotSlug': 'test_lot', 'extraneous_field': 'foo'},
+        {'slug': 'test_lot2', 'lotSlug': 'test_lot2', 'extraneous_field': 'foo'},
     ]
     mock_data_client.get_framework.return_value = {
         'frameworks': {'lots': lot_dicts, 'extraneous_field': 'foo'}, 'extraneous_field': 'foo'
@@ -117,27 +117,27 @@ def test_many_suppliers_many_lots(mock_data_client):
         if supplier_id == 123:
             # Imitates a user only in one lot.
             return iter([
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
             ])
         if supplier_id == 456:
             # Immitates a heavyweight user.
             return iter([
-                {'lot': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'test_lot', 'status': 'not-submitted', 'extraneous_field': 'foo'},
-                {'lot': 'test_lot', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'test_lot', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'test_lot2', 'status': 'submitted', 'extraneous_field': 'foo'},
-                {'lot': 'test_lot2', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'saas', 'lotSlug': 'saas', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'test_lot', 'lotSlug': 'test_lot', 'status': 'not-submitted', 'extraneous_field': 'foo'},
+                {'lot': 'test_lot', 'lotSlug': 'test_lot', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'test_lot', 'lotSlug': 'test_lot', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'test_lot2', 'lotSlug': 'test_lot2', 'status': 'submitted', 'extraneous_field': 'foo'},
+                {'lot': 'test_lot2', 'lotSlug': 'test_lot2', 'status': 'submitted', 'extraneous_field': 'foo'},
             ])
         if supplier_id == 789:
             # Imitates a supplier with no submitted service but a started application.
             return iter([{
-                'lot': 'test_lot2', 'status': 'not-submitted', 'extraneous_field': 'foo'
+                'lot': 'test_lot2', 'lotSlug': 'test_lot2', 'status': 'not-submitted', 'extraneous_field': 'foo'
             }])
         if supplier_id == 101:
             # Imitates a supplier registered but with no services.
