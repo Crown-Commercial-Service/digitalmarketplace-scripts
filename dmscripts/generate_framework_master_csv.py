e0deb32fca708336040217b29efc875e6dc4661f
benvand <benjaminfvandersteen@gmail.com>
2016-12-12 15:14:30 +0000

    Change as per Robs PR suggestions and create base class to better group methods.

--- dmscripts/generate_framework_master_csv.py
+++ dmscripts/generate_framework_master_csv.py
@@ -1,47 +1,23 @@
 # -*- coding: utf-8 -*-
 """Class used to output a framework master csv."""
 import csv
+from collections import OrderedDict
 from sys import stdout
 
 
-class GenerateMasterCSV(object):
-    """"Generate a master csv given a framework."""
+class GenerateCSVFromAPI(object):
+    """Stub class for master csv generation."""
 
-    static_fieldnames = ['supplier_id', 'supplier_dm_name', 'application_status', 'declaration_status']
-    lot_name_prefixes = ['draft', 'completed']
-
-    def __init__(self, client, target_framework_slug):
-        """Set up CSV builder with a client, framework and lot details and a placeholder for output.
-
-        :param client: Instance of dmapiclient.data.DataAPIClient
-        :param target_framework_slug: A framework slug ie 'digital-outcomes-and-specialists-2' or 'g-cloud-8'
-        """
+    def __init__(self, client):
         self.client = client
-        self.target_framework_slug = target_framework_slug
-        self.framework = self.client.get_framework(target_framework_slug)['frameworks']
-        self.lot_names = [i['slug'] for i in self.framework['lots']]
         self.output = []
 
-    def _get_dynamic_field_names(self):
-        """
-        :return: List of strings. Dynamic field names for CSV.
-        """
-        dynamic_fields = []
-        for lot_name in self.lot_names:
-            for prefix in self.lot_name_prefixes:
-                dynamic_fields.append('_'.join([prefix, lot_name]))
-        return dynamic_fields
-
     def get_fieldnames(self):
-        """
-        :return: List of strings. All field names for CSV.
-        """
-        return self.static_fieldnames + self._get_dynamic_field_names()
+        raise NotImplementedError("Required: Method for getting fieldnames.")
 
     def populate_output(self):
-        """Method to actually populate our output placeholder."""
-        self._update_with_supplier_data(self.output)
-        self._update_with_lot_data(self.output)
+        """Generally you should populate self.output here."""
+        raise NotImplementedError("Required: Method for populating output.")
 
     def write_csv(self, outfile=None):
         """Write CSV header from get_fieldnames and contents from self.output."""
@@ -50,28 +26,80 @@ class GenerateMasterCSV(object):
 
         writer.writeheader()
         for row in self.output:
-            writer.writerow({k: v for k, v in row.items()})
+            writer.writerow(row)
+
+
+class GenerateMasterCSV(GenerateCSVFromAPI):
+    """"Generate a master csv given a framework."""
+
+    static_fieldnames = ('supplier_id', 'supplier_dm_name', 'application_status', 'declaration_status')
+    service_status_labels = OrderedDict([
+        ("not-submitted", "draft"),
+        ("submitted", "completed")
+    ])
+
+    def __init__(self, client, target_framework_slug):
+        """Set up CSV builder with a client, framework and lot details and a placeholder for output.
+
+        :param client: Instance of dmapiclient.data.DataAPIClient
+        :param target_framework_slug: A framework slug ie 'digital-outcomes-and-specialists-2' or 'g-cloud-8'
+        """
+        super(GenerateMasterCSV, self).__init__(client)
+        self.target_framework_slug = target_framework_slug
+        self.framework = self.client.get_framework(target_framework_slug)['frameworks']
+        self.lot_slugs = tuple(i['slug'] for i in self.framework['lots'])
+
+    def get_column_name(self, service_status, lot_slug):
+        """For each lot we're working out how many submitted and non-submitted services they have.
+        To indicate this in the csv we have headings like 'draft_iaas, completed_iaas, draft_saas, completed_saas etc.'
+        In this method, given a status 'non-submitted' and a lot_slug 'iaas' we produce the label 'draft_iaas'.
+        """
+        return self.service_status_labels[service_status] + '_' + lot_slug
+
+    def _get_dynamic_field_names(self):
+        """
+        :return: List of strings. Dynamic field names for CSV.
+        """
+        dynamic_fields = []
+        for lot_name in self.lot_slugs:
+            for label_prefix in self.service_status_labels.keys():
+                dynamic_fields.append(self.get_column_name(label_prefix, lot_name))
+        return dynamic_fields
+
+    def get_fieldnames(self):
+        """
+        :return: List of strings. All field names for CSV.
+        """
+        return self.static_fieldnames + tuple(self._get_dynamic_field_names())
+
+    def populate_output(self):
+        """Method to actually populate our output placeholder."""
+        self._update_with_supplier_data(self.output)
 
     def get_supplier_service_data(self, supplier_id):
         """Given a supplier ID return a list of dictionaries for services related to framework."""
         return self.client.find_draft_services_iter(supplier_id, framework=self.target_framework_slug)
 
+    def get_supplier_frameworks(self):
+        """Return supplier frameworks."""
+        return self.client.find_framework_suppliers(self.target_framework_slug)['supplierFrameworks']
+
     def _update_with_supplier_data(self, output):
         """Update self.output with supplier data."""
-        supplier_frameworks = self.client.find_framework_suppliers(self.target_framework_slug)['supplierFrameworks']
+        supplier_frameworks = self.get_supplier_frameworks()
         field_names = self.get_fieldnames()
         for sf in supplier_frameworks:
+            # This bit takes care of the columns in static_fieldnames.
+            supplier_id = sf['supplierId']
             declaration = sf['declaration']['status'] if sf['declaration'] else ''
-            supplier_info = [sf['supplierId'], sf['supplierName'], '', declaration]
+            supplier_info = [supplier_id, sf['supplierName'], '', declaration]
+            # This creates placeholders for the dynamic lot fieldnames.
             lot_placeholders = [0 for i in self._get_dynamic_field_names()]
-            output.append(dict(zip(field_names, supplier_info + lot_placeholders)))
-
-    def _update_with_lot_data(self, output):
-        """Update self.output with lot data."""
-        for supplier_dict in output:
-            service_data = self.get_supplier_service_data(supplier_dict['supplier_id'])
+            supplier_dict = dict(zip(field_names, supplier_info + lot_placeholders))
+            # Get service data and process dynamic lot values
+            service_data = self.get_supplier_service_data(supplier_id)
             for service in service_data:
-                if service['status'] == 'submitted':
-                    supplier_dict['completed_' + service['lot']] += 1
-                elif service['status'] == 'not-submitted':
-                    supplier_dict['draft_' + service['lot']] += 1
+                # Calculate the status of each service an what lot it is in then +1 to the corresponding column.
+                column_name = self.get_column_name(service['status'], service['lotSlug'])
+                supplier_dict[column_name] += 1
+            output.append(supplier_dict)
