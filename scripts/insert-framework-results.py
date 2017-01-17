937cd4beb3b377859cf49b91e3d9aef57dc7b04d
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-01-17 16:16:38 +0000

    Use API endpoint lookup method
    Rather than having to know the full API URL.

--- scripts/insert-framework-results.py
+++ scripts/insert-framework-results.py
@@ -5,8 +5,11 @@ e.g:
 123212, Supplier name 2, fail
 234567, Supplier name 3, pass
 
+The supplier name is cross-referenced against the supplier name in the Digital Marketplace
+for that supplier ID. If the names don't match then the script will not update the framework status.
+
 Usage:
-    scripts/insert-framework-results.py <framework_slug> <data_api_url> <data_api_token> <filename>
+    scripts/insert-framework-results.py <framework_slug> <stage> <data_api_token> <filename> [<user>]
 """
 
 import getpass
@@ -15,16 +18,17 @@ sys.path.insert(0, '.')
 
 from docopt import docopt
 from dmapiclient import DataAPIClient
+from dmscripts.env import get_api_endpoint_from_stage
 from dmscripts.insert_framework_results import insert_results
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
 
-    client = DataAPIClient(arguments['<data_api_url>'], arguments['<data_api_token>'])
+    client = DataAPIClient(get_api_endpoint_from_stage(arguments['<stage>']), arguments['<data_api_token>'])
     output = sys.stdout
     framework_slug = arguments['<framework_slug>']
     filename = arguments['<filename>']
-    user = getpass.getuser()
+    user = arguments['<user>'] or getpass.getuser()
 
     insert_results(client, output, framework_slug, filename, user)
