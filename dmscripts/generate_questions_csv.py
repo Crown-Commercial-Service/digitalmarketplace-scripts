2a9595437d5695ac1fbd63679ac1e28001c06445
George Lund <george.lund@digital.cabinet-office.gov.uk>
2017-02-03 15:21:46 +0000

    Allow question-set to be specified on the command line.
     - if we're restricting by lot, it makes sense to be able to get
       separate downloads for each question-set too;
     - at this point, a pre-generated name makes no sense, and figuring
       one out would require inspecting an arbitrarily-complex 'context'
       dictionary, so we now have the user pick the output file name.

--- dmscripts/generate_questions_csv.py
+++ dmscripts/generate_questions_csv.py
@@ -6,6 +6,7 @@ if sys.version_info > (3, 0):
     import csv
 else:
     import unicodecsv as csv
+import os.path
 
 MANIFESTS = [{
     'question_set': 'declaration',
@@ -74,21 +75,22 @@ def return_rows_for_sections(sections):
     return local_rows
 
 
-def generate_csv(output_directory, framework_slug, content_loader, context):
-
+def generate_csv(output_file, framework_slug, content_loader, question_set, context):
+    output_directory = os.path.dirname(output_file)
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
     rows = []
 
     for manifest in MANIFESTS:
-        content_loader.load_manifest(framework_slug, manifest['question_set'], manifest['manifest'])
+        if not question_set or question_set == manifest['question_set']:
+            content_loader.load_manifest(framework_slug, manifest['question_set'], manifest['manifest'])
 
-        content = content_loader.get_manifest(framework_slug, manifest['manifest'])
-        if context is not None:
-            content = content.filter(context)
+            content = content_loader.get_manifest(framework_slug, manifest['manifest'])
+            if context is not None:
+                content = content.filter(context)
 
-        rows.extend(return_rows_for_sections(content.sections))
+            rows.extend(return_rows_for_sections(content.sections))
 
     # find the longest array
     max_length = max(len(row) for row in rows)
@@ -98,7 +100,14 @@ def generate_csv(output_directory, framework_slug, content_loader, context):
     for index, row in enumerate(rows):
         row.extend([''] * (max_length - len(row)))
 
-    with open('{}/{}-questions.csv'.format(output_directory, framework_slug), 'wb') as csvfile:
+    filename = framework_slug
+    if question_set:
+        filename += '-' + question_set
+    if context:
+        for v in context.values():
+            filename += '-' + v
+
+    with open(output_file, 'wb') as csvfile:
         writer = csv.writer(csvfile, delimiter=b',', quotechar=b'"')
         header = ["Page title", "Page title hint", "Question", "Hint"]
         header.extend(
