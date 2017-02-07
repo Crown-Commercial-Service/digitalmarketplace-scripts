66c738610456720caae5fdd7860eaed2d97159a1
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-02-07 16:15:39 +0000

    Add methods to find all suppliers on a framework with details
    These are of use to several scripts - getting all suppliers on a framework and collating info from their declaration, draft servie counts, etc.

--- dmscripts/helpers/framework_helpers.py
+++ dmscripts/helpers/framework_helpers.py
@@ -1,3 +1,7 @@
+from collections import Counter
+from functools import partial
+from multiprocessing.pool import ThreadPool
+
 from dmapiclient import HTTPError
 
 
@@ -32,3 +36,65 @@ def find_suppliers_on_framework(client, framework_slug):
         supplier for supplier in client.find_framework_suppliers(framework_slug)['supplierFrameworks']
         if supplier['onFramework']
     )
+
+
+def find_suppliers_with_details_and_draft_services(client, framework_slug, supplier_ids=None, lot=None, statuses=None):
+    pool = ThreadPool(30)
+
+    records = find_suppliers(client, framework_slug, supplier_ids)
+    records = pool.imap(partial(add_supplier_info, client), records)
+    records = pool.imap(partial(add_framework_info, client, framework_slug), records)
+    records = pool.imap(partial(add_draft_services, client, framework_slug, lot=lot, statuses=statuses), records)
+    records = filter(lambda record: len(record["services"]) > 0, records)
+    return records
+
+
+def find_suppliers_with_details_and_draft_service_counts(client, framework_slug, supplier_ids=None):
+    pool = ThreadPool(30)
+
+    records = find_suppliers(client, framework_slug, supplier_ids)
+    records = pool.imap(partial(add_supplier_info, client), records)
+    records = pool.imap(partial(add_framework_info, client, framework_slug), records)
+    records = pool.imap(partial(add_draft_counts, client, framework_slug), records)
+    return records
+
+
+def find_suppliers(client, framework_slug, supplier_ids=None):
+    suppliers = client.get_interested_suppliers(framework_slug)['interestedSuppliers']
+    return ({'supplier_id': supplier_id} for supplier_id in suppliers
+            if (supplier_ids is None) or (supplier_id in supplier_ids))
+
+
+def add_supplier_info(client, record):
+    supplier = client.get_supplier(record['supplier_id'])
+    return dict(record, supplier=supplier['suppliers'])
+
+
+def add_framework_info(client, framework_slug, record):
+    supplier_framework = client.get_supplier_framework_info(record['supplier_id'], framework_slug)['frameworkInterest']
+    return dict(record,
+                onFramework=supplier_framework['onFramework'],
+                declaration=supplier_framework['declaration'] or {},
+                countersignedPath=supplier_framework['countersignedPath'] or "",
+                countersignedAt=supplier_framework['countersignedAt'] or "",
+                )
+
+
+def add_draft_services(client, framework_slug, record, lot=None, statuses=None):
+        drafts = client.find_draft_services(record["supplier_id"], framework=framework_slug)
+        drafts = [
+            draft for draft in drafts["services"]
+            if (not lot or draft["lotSlug"] == lot) and (not statuses or draft["status"] in statuses)
+            ]
+        return dict(record, services=drafts)
+
+
+def add_draft_counts(client, framework_slug, record):
+    # "counts" is a counter of (lotSlug, status) tuples
+    counts = Counter(
+        (ds['lot'], ds['status'])
+        for ds in client.find_draft_services_iter(record['supplier']['id'], framework=framework_slug)
+    )
+    print("RECORD IN HELPER: {}".format(record))
+    print("GOT COUNTS IN HELPER: {}".format(counts))
+    return dict(record, counts=counts)
