8b1b2c0aa8104accdbb11a0901a808d819c5ee19
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-02-10 15:35:41 +0000

    Rename and add comment to describe purpose of bad-words script

--- scripts/scan-g-cloud-drafts-for-bad-words.py
+++ scripts/scan-g-cloud-drafts-for-bad-words.py
@@ -1,7 +1,10 @@
 """
+This script will check all free-text fields in submitted G-Cloud draft services for "bad words", as defined in
+the file at <bad_words_path> (typically blacklist.txt in https://github.gds/gds/digitalmarketplace-bad-words),
+and generate a CSV report of any bad word found.
 
 Usage:
-    scripts/bad_words.py <stage> <api_token> <bad_words_path> <framework_slug> <output_dir>
+    scripts/scan-g-cloud-drafts-for-bad-words.py <stage> <api_token> <bad_words_path> <framework_slug> <output_dir>
 """
 
 import sys
