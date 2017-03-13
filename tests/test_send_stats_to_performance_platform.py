05a8de775d569317881ffac106a29895a3b3e2a0
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2017-03-13 11:31:36 +0000

    Appease Python 3 more
    Lists generated in differnt order for Py2 and Py3 so check calls independent of order of list.

--- tests/test_send_stats_to_performance_platform.py
+++ tests/test_send_stats_to_performance_platform.py
@@ -158,45 +158,53 @@ def test_services_by_lot():
 @mock.patch('dmscripts.send_stats_to_performance_platform.send_data')
 def test_send_by_stage_stats_per_day_calls_send_data_with_correct_data(send_data):
     send_by_stage_stats(STATS_JSON, '2017-03-29T00:00:00+00:00', 'day', 'pp-bearer-token')
+    expected_sent_data_items = [
+        {'count': 184,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'day',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-interested'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1pbnRlcmVzdGVk',
+         'stage': 'interested'
+        },
+        {'count': 97,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'day',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-made-declaration'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1tYWRlLWRlY2xhcmF0aW9u',  # noqa
+         'stage': 'made-declaration'
+         },
+        {'count': 79,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'day',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-completed-services'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1jb21wbGV0ZWQtc2VydmljZXM=',  # noqa
+         'stage': 'completed-services'
+         },
+        {'count': 89,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'day',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-eligible'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1lbGlnaWJsZQ==',
+         'stage': 'eligible'
+         }
+    ]
+    # Python 2 and Python 3 can generate the data in varying order, so test items independent of order in the list
+    sent_data = send_data.call_args[0][0]
+    assert len(sent_data) == 4
+    for item in sent_data:
+        assert item in expected_sent_data_items
+        expected_sent_data_items.remove(item)  # Each item should appear once in the call args so remove once found
+
     send_data.assert_called_with(
-        [
-            {'count': 184,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'day',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-interested'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1pbnRlcmVzdGVk',
-             'stage': 'interested'
-            },
-            {'count': 97,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'day',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-made-declaration'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1tYWRlLWRlY2xhcmF0aW9u',  # noqa
-             'stage': 'made-declaration'
-             },
-            {'count': 79,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'day',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-completed-services'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1jb21wbGV0ZWQtc2VydmljZXM=',  # noqa
-             'stage': 'completed-services'
-             },
-            {'count': 89,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'day',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-stage-eligible'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1zdGFnZS1lbGlnaWJsZQ==',
-             'stage': 'eligible'
-             }
-        ],
+        mock.ANY,  # Data sent has already been tested above
         'https://www.performance.service.gov.uk/data/gcloud/applications-by-stage',
         'pp-bearer-token'
     )
@@ -205,45 +213,53 @@ def test_send_by_stage_stats_per_day_calls_send_data_with_correct_data(send_data
 @mock.patch('dmscripts.send_stats_to_performance_platform.send_data')
 def test_send_by_stage_stats_per_hour_calls_send_data_with_correct_data(send_data):
     send_by_stage_stats(STATS_JSON, '2017-03-29T12:00:00+00:00', 'hour', 'pp-bearer-token')
+    expected_sent_data_items = [
+        {'count': 184,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'hour',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-interested'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtaW50ZXJlc3RlZA==',
+         'stage': 'interested'
+         },
+        {'count': 97,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'hour',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-made-declaration'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtbWFkZS1kZWNsYXJhdGlvbg==',  # noqa
+         'stage': 'made-declaration'
+         },
+        {'count': 79,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'hour',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-completed-services'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtY29tcGxldGVkLXNlcnZpY2Vz',  # noqa
+         'stage': 'completed-services'
+         },
+        {'count': 89,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-stage',
+         'period': 'hour',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-eligible'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtZWxpZ2libGU=',
+         'stage': 'eligible'
+         }
+    ]
+    # Python 2 and Python 3 can generate the data in varying order, so test items independent of order in the list
+    sent_data = send_data.call_args[0][0]
+    assert len(sent_data) == 4
+    for item in sent_data:
+        assert item in expected_sent_data_items
+        expected_sent_data_items.remove(item)  # Each item should appear once in the call args so remove once found
+
     send_data.assert_called_with(
-        [
-            {'count': 184,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'hour',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-interested'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtaW50ZXJlc3RlZA==',
-             'stage': 'interested'
-             },
-            {'count': 97,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'hour',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-made-declaration'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtbWFkZS1kZWNsYXJhdGlvbg==',  # noqa
-             'stage': 'made-declaration'
-             },
-            {'count': 79,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'hour',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-completed-services'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtY29tcGxldGVkLXNlcnZpY2Vz',  # noqa
-             'stage': 'completed-services'
-             },
-            {'count': 89,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-stage',
-             'period': 'hour',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-stage-eligible'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktc3RhZ2UtZWxpZ2libGU=',
-             'stage': 'eligible'
-             }
-        ],
+        mock.ANY,  # Data sent has already been tested above
         'https://www.performance.service.gov.uk/data/gcloud/applications-by-stage-realtime',
         'pp-bearer-token'
     )
@@ -252,36 +268,45 @@ def test_send_by_stage_stats_per_hour_calls_send_data_with_correct_data(send_dat
 @mock.patch('dmscripts.send_stats_to_performance_platform.send_data')
 def test_send_by_lot_stats_per_day_calls_send_data_with_correct_data(send_data):
     send_by_lot_stats(STATS_JSON, '2017-03-29T00:00:00+00:00', 'day', FRAMEWORK_JSON['frameworks'], 'pp-bearer-token')
+    expected_sent_data_items = [
+        {'count': 84,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-lot',
+         'period': 'day',
+         'lot': 'cloud-hosting',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-lot-cloud-hosting'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1sb3QtY2xvdWQtaG9zdGluZw=='
+         },
+        {'count': 102,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-lot',
+         'period': 'day',
+         'lot': 'cloud-support',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-lot-cloud-support'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1sb3QtY2xvdWQtc3VwcG9ydA=='
+         },
+        {'count': 94,
+         '_timestamp': '2017-03-29T00:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-lot',
+         'period': 'day',
+         'lot': 'cloud-software',
+         # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-lot-cloud-software'
+         '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1sb3QtY2xvdWQtc29mdHdhcmU='
+         }
+    ]
+
+    # Python 2 and Python 3 can generate the data in varying order, so test items independent of order in the list
+    sent_data = send_data.call_args[0][0]
+    assert len(sent_data) == 3
+    for item in sent_data:
+        assert item in expected_sent_data_items
+        expected_sent_data_items.remove(item)  # Each item should appear once in the call args so remove once found
+
     send_data.assert_called_with(
-        [
-            {'count': 84,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-lot',
-             'period': 'day',
-             'lot': 'cloud-hosting',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-lot-cloud-hosting'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1sb3QtY2xvdWQtaG9zdGluZw=='
-             },
-            {'count': 102,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-lot',
-             'period': 'day',
-             'lot': 'cloud-support',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-lot-cloud-support'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1sb3QtY2xvdWQtc3VwcG9ydA=='
-             },
-            {'count': 94,
-             '_timestamp': '2017-03-29T00:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-lot',
-             'period': 'day',
-             'lot': 'cloud-software',
-             # Base 64 encoding of '2017-03-29T00:00:00+00:00-gcloud-day-applications-by-lot-cloud-software'
-             '_id': 'MjAxNy0wMy0yOVQwMDowMDowMCswMDowMC1nY2xvdWQtZGF5LWFwcGxpY2F0aW9ucy1ieS1sb3QtY2xvdWQtc29mdHdhcmU='
-             }
-        ],
+        mock.ANY,  # Data sent has already been tested above
         'https://www.performance.service.gov.uk/data/gcloud/applications-by-lot',
         'pp-bearer-token'
     )
@@ -290,36 +315,45 @@ def test_send_by_lot_stats_per_day_calls_send_data_with_correct_data(send_data):
 @mock.patch('dmscripts.send_stats_to_performance_platform.send_data')
 def test_send_by_lot_stats_per_hour_calls_send_data_with_correct_data(send_data):
     send_by_lot_stats(STATS_JSON, '2017-03-29T12:00:00+00:00', 'hour', FRAMEWORK_JSON['frameworks'], 'pp-bearer-token')
+    expected_sent_data_items = [
+        {'count': 84,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-lot',
+         'period': 'hour',
+         'lot': 'cloud-hosting',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-lot-cloud-hosting'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktbG90LWNsb3VkLWhvc3Rpbmc='
+         },
+        {'count': 102,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-lot',
+         'period': 'hour',
+         'lot': 'cloud-support',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-lot-cloud-support'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktbG90LWNsb3VkLXN1cHBvcnQ='
+         },
+        {'count': 94,
+         '_timestamp': '2017-03-29T12:00:00+00:00',
+         'service': 'gcloud',
+         'dataType': 'applications-by-lot',
+         'period': 'hour',
+         'lot': 'cloud-software',
+         # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-lot-cloud-software'
+         '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktbG90LWNsb3VkLXNvZnR3YXJl'
+         }
+    ]
+
+    # Python 2 and Python 3 can generate the data in varying order, so test items independent of order in the list
+    sent_data = send_data.call_args[0][0]
+    assert len(sent_data) == 3
+    for item in sent_data:
+        assert item in expected_sent_data_items
+        expected_sent_data_items.remove(item)  # Each item should appear once in the call args so remove once found
+
     send_data.assert_called_with(
-        [
-            {'count': 84,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-lot',
-             'period': 'hour',
-             'lot': 'cloud-hosting',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-lot-cloud-hosting'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktbG90LWNsb3VkLWhvc3Rpbmc='
-             },
-            {'count': 102,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-lot',
-             'period': 'hour',
-             'lot': 'cloud-support',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-lot-cloud-support'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktbG90LWNsb3VkLXN1cHBvcnQ='
-             },
-            {'count': 94,
-             '_timestamp': '2017-03-29T12:00:00+00:00',
-             'service': 'gcloud',
-             'dataType': 'applications-by-lot',
-             'period': 'hour',
-             'lot': 'cloud-software',
-             # Base 64 encoding of '2017-03-29T12:00:00+00:00-gcloud-hour-applications-by-lot-cloud-software'
-             '_id': 'MjAxNy0wMy0yOVQxMjowMDowMCswMDowMC1nY2xvdWQtaG91ci1hcHBsaWNhdGlvbnMtYnktbG90LWNsb3VkLXNvZnR3YXJl'
-             }
-        ],
+        mock.ANY,  # Data sent has already been tested above
         'https://www.performance.service.gov.uk/data/gcloud/applications-by-lot-realtime',
         'pp-bearer-token'
     )
