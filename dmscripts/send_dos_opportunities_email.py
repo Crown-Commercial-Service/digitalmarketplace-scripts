7b7ccf9c70de12b383ca5aa04fb655b9bafbcc56
David McDonald <idavidmcdonald@me.com>
2017-04-03 14:24:45 +0100

    Add `create_campaign_data`, `create_campaign` & `set_campaign_content`
    Continued from the spike into mailchimp integration and have
    formalised the above mentioned functions with tests. We have
    added error logging for failed API calls. We have set up
    a dictionary of lots and their related data.
    
    Note, not able to use `autospec` for mailchimp client because
    attributes are created at init:
    http://stackoverflow.com/questions/35915703/when-using-unittest-mock-patch-why-is-autospec-not-true-by-default

--- dmscripts/send_dos_opportunities_email.py
+++ dmscripts/send_dos_opportunities_email.py
@@ -1,66 +1,105 @@
-"""Spike script into mail chimp email sending."""
+"""Spike script into mail chimp email sending.
+
+Test list ID is "096e52cebb" which can be used for local development
+"""
+from datetime import datetime
+
 from mailchimp3 import MailChimp
 
-from dmscripts.helpers.html_helpers import render_html
+from requests.exceptions import RequestException
 
-SPECIALISTS_CAMPAIGN_DATA = {
-	"type": "regular",
-	"recipients": {
-		"list_id": "096e52cebb",  # test list containing one user (david mcdonald work email)
-	},
-	"settings": {
-		"subject_line": "New opportunities for Digital Specialists: Digital Outcomes and Specialists 2",
-		"title": "TEST! DOS Suppliers: Specialists [16 March]",
-		"from_name": "Digital Marketplace Team",
-		"reply_to": "do-not-reply@digitalmarketplace.service.gov.uk",
-		"use_conversation": False,
-		"authenticate": True,
-		"auto_footer": False,
-		"inline_css": False,
-		"auto_tweet": False,
-		"fb_comments": False
-	},
-	"tracking": {
-		"opens": True,
-		"html_clicks": True,
-		"text_clicks": False,
-		"goal_tracking": False,
-		"ecomm360": False
-	}
+from dmscripts.helpers.html_helpers import render_html
+from dmscripts.helpers import logging_helpers
+from dmscripts.helpers.logging_helpers import logging
+
+logger = logging_helpers.configure_logger({'dmapiclient': logging.INFO})
+
+
+lots = {
+    "digital-specialists": {
+        "lot_name": "Digital specialists",
+        "list_id": "096e52cebb"
+    },
+    "digital-outcomes": {
+        "lot_name": "Digital outcomes",
+        "list_id": "096e52cebb"
+    },
+    "user-research-participants": {
+        "lot_name": "User research participants",
+        "list_id": "096e52cebb"
+    }
 }
 
+
+def create_campaign_data(lot_name, list_id):
+    return {
+        "type": "regular",
+        "recipients": {
+            "list_id": list_id,
+        },
+        "settings": {
+            "subject_line": "New opportunities for {0}: Digital Outcomes and Specialists 2".format(lot_name),
+            "title": "DOS Suppliers: {0} [{1}]".format(lot_name, datetime.now().strftime("%d %B")),
+            "from_name": "Digital Marketplace Team",
+            "reply_to": "do-not-reply@digitalmarketplace.service.gov.uk",
+            "use_conversation": False,
+            "authenticate": True,
+            "auto_footer": False,
+            "inline_css": False,
+            "auto_tweet": False,
+            "fb_comments": False
+        },
+        "tracking": {
+            "opens": True,
+            "html_clicks": True,
+            "text_clicks": False,
+            "goal_tracking": False,
+            "ecomm360": False
+        }
+    }
+
+
 def get_html_content():
-	email_body = render_html('email_templates/dos_opportunities.html', data={})
-	return email_body
+    email_body = render_html("email_templates/dos_opportunities.html", data={})
+    return email_body
 
-def create_campaign(mailchimp_client):
-	try:
-		res = mailchimp_client.campaigns.create(data=SPECIALISTS_CAMPAIGN_DATA)
-		campaign_id = res['id']
-	except Exception as e:
-		print e
 
-	SPECIALISTS_CONTENT_DATA = {
-		"html": get_html_content()
-	}
+def create_campaign(mailchimp_client, campaign_data):
+    try:
+        campaign = mailchimp_client.campaigns.create(campaign_data)
+        return campaign['id']
+    except RequestException as e:
+        logger.error(
+            "Mailchimp failed to create campaign for '{0}'".format(
+                campaign_data.get("settings").get("title")
+            ),
+            extra={"error": e.message}
+        )
+    return False
 
-	try:
-		mailchimp_client.campaigns.content.update(campaign_id=campaign_id, data=SPECIALISTS_CONTENT_DATA)
-	except Exception as e:
-		print e
 
-	return campaign_id
+def set_campaign_content(mailchimp_client, campaign_id, content_data):
+    try:
+        return mailchimp_client.campaigns.content.update(campaign_id, content_data)
+    except RequestException as e:
+        logger.error(
+            "Mailchimp failed to set content for campaign id '{0}'".format(campaign_id),
+            extra={"error": e.message}
+        )
+    return False
+
 
 def send_campaign(mailchimp_client, campaign_id):
-	try:
-		return client.campaigns.actions.send(campaign_id=campaign_id)
-	except Exception as e:
-		print e
+    try:
+        return client.campaigns.actions.send(campaign_id=campaign_id)
+    except Exception as e:
+        print e
+
 
 def main(mailchimp_username, mailchimp_api_key):
-	mailchimp_client = MailChimp(mailchimp_username, mailchimp_api_key)
+    mailchimp_client = MailChimp(mailchimp_username, mailchimp_api_key)
 
-	campaign_id = create_campaign(mailchimp_client)
-	if not campaign_id:
-		return False
-	return True
+    campaign_id = create_campaign(mailchimp_client)
+    if not campaign_id:
+        return False
+    return True
---
+++ tests/test_send_dos_opportunities_email.py
@@ -0,0 +1,78 @@
+import pytest
+import mock
+from freezegun import freeze_time
+from requests.exceptions import RequestException
+
+import datetime
+
+from dmscripts.send_dos_opportunities_email import (
+    main,
+    create_campaign_data,
+    create_campaign,
+    set_campaign_content
+)
+
+
+def test_create_campaign_data():
+    lot_name = "Digital Outcomes"
+    list_id = "1111111"
+    expected_subject = "New opportunities for Digital Outcomes: Digital Outcomes and Specialists 2"
+
+    with freeze_time('2017-04-19 08:00:00'):
+        campaign_data = create_campaign_data(lot_name, list_id)
+        assert campaign_data["recipients"]["list_id"] == list_id
+        assert campaign_data["settings"]["subject_line"] == expected_subject
+        assert campaign_data["settings"]["title"] == "DOS Suppliers: Digital Outcomes [19 April]"
+        assert campaign_data["settings"]["from_name"] == "Digital Marketplace Team"
+        assert campaign_data["settings"]["reply_to"] == "do-not-reply@digitalmarketplace.service.gov.uk"
+
+
+@mock.patch('dmscripts.send_dos_opportunities_email.MailChimp')
+def test_create_campaign(mailchimp_client):
+    mailchimp_client.campaigns.create.return_value = {"id": "100"}
+
+    res = create_campaign(mailchimp_client, {"example": "data"})
+    assert res == "100"
+    mailchimp_client.campaigns.create.assert_called_once_with({"example": "data"})
+
+
+@mock.patch('dmscripts.send_dos_opportunities_email.logger', autospec=True)
+@mock.patch('dmscripts.send_dos_opportunities_email.MailChimp')
+def test_log_error_message_if_error_creating_campaign(mailchimp_client, logger):
+    mailchimp_client.campaigns.create.side_effect = RequestException("error message")
+    campaign_data = {
+        "example": "data",
+        "settings": {
+            "title": "campaign title"
+        }
+    }
+
+    assert create_campaign(mailchimp_client, campaign_data) is False
+    logger.error.assert_called_once_with(
+        "Mailchimp failed to create campaign for 'campaign title'", extra={"error": "error message"}
+    )
+
+
+@mock.patch('dmscripts.send_dos_opportunities_email.MailChimp')
+def test_set_campaign_content(mailchimp_client):
+    campaign_id = "1"
+    html_content = {"html": "<p>One or two words</p>"}
+    mailchimp_client.campaigns.content.update.return_value = html_content
+
+    res = set_campaign_content(mailchimp_client, campaign_id, html_content)
+    assert res == html_content
+    mailchimp_client.campaigns.content.update.assert_called_once_with(campaign_id, html_content)
+
+
+@mock.patch('dmscripts.send_dos_opportunities_email.logger', autospec=True)
+@mock.patch('dmscripts.send_dos_opportunities_email.MailChimp')
+def test_log_error_message_if_error_setting_campaign_content(mailchimp_client, logger):
+    mailchimp_client.campaigns.content.update.side_effect = RequestException("error message")
+    content_data = {
+        "html": "some html"
+    }
+
+    assert set_campaign_content(mailchimp_client, 1, content_data) is False
+    logger.error.assert_called_once_with(
+        "Mailchimp failed to set content for campaign id '1'", extra={"error": "error message"}
+    )
