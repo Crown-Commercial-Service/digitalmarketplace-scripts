d79cb3d4d819ef62296b6fd1df519b30a853944c
David McDonald <idavidmcdonald@me.com>
2017-04-07 16:50:06 +0100

    Move lot set up and for loop from main to script

--- scripts/send_dos_opportunities_email.py
+++ scripts/send_dos_opportunities_email.py
@@ -14,13 +14,33 @@ from docopt import docopt
 sys.path.insert(0, '.')
 from dmscripts.send_dos_opportunities_email import main
 
+lots = [
+    {
+        "lot_slug": "digital-specialists",
+        "lot_name": "Digital specialists",
+        "list_id": "096e52cebb"
+    },
+    {
+        "lot_slug": "digital-outcomes",
+        "lot_name": "Digital outcomes",
+        "list_id": "096e52cebb"
+    },
+    {
+        "lot_slug": "user-research-participants",
+        "lot_name": "User research participants",
+        "list_id": "096e52cebb"
+    }
+]
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
-    ok = main(
-        mailchimp_username=arguments['--mailchimp-username'],
-        mailchimp_api_key=arguments['--mailchimp-api-key']
-    )
+    lots = ['ds', 'do', 'urp']
+    for lot in lots:
+        ok = main(
+            mailchimp_username=arguments['--mailchimp-username'],
+            mailchimp_api_key=arguments['--mailchimp-api-key'],
+            lot=lot
+        )
 
     if not ok:
         sys.exit(1)
