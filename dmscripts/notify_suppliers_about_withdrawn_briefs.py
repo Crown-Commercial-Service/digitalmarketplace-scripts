f95ad244476660936587c8d1b37d0cc44951168e
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2017-10-23 17:57:45 +0100

    Get brief responses for briefs withdrawn on a given date

--- dmscripts/notify_suppliers_about_withdrawn_briefs.py
+++ dmscripts/notify_suppliers_about_withdrawn_briefs.py
@@ -1,7 +1,22 @@
-import dmapiclient
+from dmapiclient import DataAPIClient as data_api_client
 from datetime import datetime, date, timedelta
 
-def main():
-    briefs_withdrawn_on = date.today() - timedelta(days=1)
-    dmapiclient.DataAPIClient.find_briefs_by_status_datestamp("withdrawn_at", briefs_withdrawn_on, briefs_withdrawn_on, inclusive=True)
+
+def get_brief_responses(data_api_client, brief):
+    return data_api_client.find_brief_responses(brief_id=brief["id"], status="submitted").get("briefResponses")
+
+
+def get_withdrawn_briefs_with_responses(data_api_client, withdrawn_briefs):
+    return {brief["id"]: get_brief_responses(data_api_client, brief) for brief in withdrawn_briefs}
+
+
+def main(data_api_client):
+    withdrawn_date = date.today() - timedelta(days=1)
+    briefs_withdrawn_on_date = data_api_client.find_briefs_by_status_datestamp(
+        "withdrawn_at",
+        withdrawn_date,
+        withdrawn_date,
+        inclusive=True
+    ).get("briefs")
+    withdrawn_briefs_with_responses = get_withdrawn_briefs_with_responses(data_api_client, briefs_withdrawn_on_date)
     return True
--- tests/test_notify_suppliers_about_withdrawn_briefs.py
+++ tests/test_notify_suppliers_about_withdrawn_briefs.py
@@ -9,10 +9,39 @@ from dmscripts import notify_suppliers_about_withdrawn_briefs as tested_script
 
 
 class TestMain:
-    @mock.patch('dmscripts.notify_suppliers_about_withdrawn_briefs.dmapiclient.DataAPIClient')
-    def test_main_calls_functions(self, data_api_client):
+
+    @mock.patch('dmscripts.notify_suppliers_about_withdrawn_briefs.data_api_client')
+    def test_get_brief_responses(self, data_api_client):
+        data_api_client.find_brief_responses.return_value = {"briefResponses": [{"id": 4321}, {"id": 4389}]}
+        assert tested_script.get_brief_responses(data_api_client, {"id": 1234}) == [{"id": 4321}, {"id": 4389}]
+
+    @mock.patch('dmscripts.notify_suppliers_about_withdrawn_briefs.get_brief_responses')
+    def test_get_withdrawn_briefs_with_responses(self, get_brief_responses):
+        get_brief_responses.side_effect = [
+            [{"id": 4321}, {"id": 4389}],
+            [],
+        ]
+        briefs = [
+            {"id": 123, "withdrawnAt": "2016-01-28 16:23:50.618053"},
+            {"id": 235, "withdrawnAt": "2016-01-28 08:23:50.618053"}
+        ]
+        briefs_with_responses = {123: [{"id": 4321}, {"id": 4389}], 235: []}
+        assert tested_script.get_withdrawn_briefs_with_responses(mock.Mock(), briefs) == briefs_with_responses
+
+    @mock.patch('dmscripts.notify_suppliers_about_withdrawn_briefs.get_withdrawn_briefs_with_responses')
+    @mock.patch('dmscripts.notify_suppliers_about_withdrawn_briefs.data_api_client')
+    def test_main_calls_functions(self, data_api_client, get_withdrawn_briefs_with_responses):
+        withdrawn_briefs = [
+            {"id": 123, "withdrawnAt": "2016-01-28 16:23:50.618053"},
+            {"id": 235, "withdrawnAt": "2016-01-28 08:23:50.618053"}
+        ]
+        data_api_client.find_briefs_by_status_datestamp.return_value = {"briefs": withdrawn_briefs}
+        get_withdrawn_briefs_with_responses.return_value = {123: [{"id": 4321}, {"id": 4389}], 235: []}
         with freeze_time('2016-01-29 03:04:05'):
-            tested_script.main()
-            yesterday = date(2016,1,28)
+            tested_script.main(data_api_client)
+            yesterday = date(2016, 1, 28)
             expected_args = [mock.call('withdrawn_at', yesterday, yesterday, inclusive=True)]
             assert data_api_client.find_briefs_by_status_datestamp.call_args_list == expected_args
+            assert tested_script.get_withdrawn_briefs_with_responses.call_args_list == [mock.call(
+                data_api_client, withdrawn_briefs
+            )]
