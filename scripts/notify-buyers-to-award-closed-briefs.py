f4c24c4772c5b4e8cd0122eafa60f4af50ce8fbd
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-07-03 12:33:44 +0100

    scripts/notify-buyers-to-award-closed-briefs.py
     ## Summary
    Use `get_auth_token` to retrieve API token from environment rather than
    pass in on command line.

--- scripts/notify-buyers-to-award-closed-briefs.py
+++ scripts/notify-buyers-to-award-closed-briefs.py
@@ -2,11 +2,10 @@
 """Send email notifications to buyer users to remind them to award their closed requirements.
 
 Usage:
-    notify-buyers-to-award-closed-briefs.py <stage> <api_token>
-        <govuk_notify_api_key> <govuk_notify_template_id> [options]
+    notify-buyers-to-award-closed-briefs.py <stage> <govuk_notify_api_key> <govuk_notify_template_id> [options]
 
 Example:
-    notify-buyers-to-award-closed-briefs.py local myToken myNotifyToken myNotifyTemplateId --dry-run=True --buyer-ids=1
+    notify-buyers-to-award-closed-briefs.py local myNotifyToken myNotifyTemplateId --dry-run=True --buyer-ids=1
 
 Options:
     -h, --help  Show this screen
@@ -19,10 +18,11 @@ Options:
 
 import sys
 
-sys.path.insert(0, '.')
+sys.path.insert(0, '.')  # noqa
 from docopt import docopt
 
 from dmscripts.helpers.env_helpers import get_api_endpoint_from_stage
+from dmscripts.helpers.auth_helpers import get_auth_token
 from dmscripts.notify_buyers_to_award_closed_briefs import main
 
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
     ok = main(
         data_api_url=get_api_endpoint_from_stage(arguments['<stage>'], 'api'),
-        data_api_access_token=arguments['<api_token>'],
+        data_api_access_token=get_auth_token('api', arguments['<stage>']),
         notify_api_key=arguments['<govuk_notify_api_key>'],
         notify_template_id=arguments['<govuk_notify_template_id>'],
         date_closed=arguments['--date-closed'],
