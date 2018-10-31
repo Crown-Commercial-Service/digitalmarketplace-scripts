feb49c484ca44b901e451ab44ffe40e35cd8e94a
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-10-31 16:39:41 +0000

    Completes class of methods for getting rid of declarations
    This commit contains a class that exposes a couple of methods for getting rid of supplier declarations. It is written as functionally as possible.

--- dmscripts/supplier_framework.py
+++ dmscripts/supplier_framework.py
@@ -7,16 +7,16 @@ endpoint on /suppliers/<supplier_id>/frameworks/>framework_slug>
 This script gets all the suppliers that can have their declarations removed because they did not get onto a
 specified framework, identifies their ids, and then calls the endpoint to remove the declaration
 """
-import sys
-
-sys.path.insert(0, '.')
+from datetime import date, timedelta
 
 
 class SupplierFrameworkMethods:
 
-    def __init__(self, api_client, framework_slug: str):
+    def __init__(self, api_client, logger, framework_slug: str=None, dry_run: bool=True):
         self.api_client = api_client
         self.framework_slug = framework_slug
+        self.dry_run = dry_run
+        self.logger = logger
 
     def suppliers_application_failed_to_framework(self):
         """
@@ -31,24 +31,60 @@ class SupplierFrameworkMethods:
             if framework_supplier['onFramework'] == 'False'
         ]
 
-    def remove_declaration(self, supplier_id: int):
+    def remove_declaration(self, supplier_id: int, framework_slug: str):
         """
-        This method accesses an endpoint and removes the declaration of the associated SupplierFramework.
+        This method accesses an endpoint and removes the declaration of the associated SupplierFramework. It returns
+        either the response object from the endpoint, or throws an exception
         :param supplier_id: the identifier for the supplier whose declaration should be removed
         :type supplier_id: int
-        :return: True if successful; else False
-        :rtype: bool
+        :param framework_slug: the string representation of the framework to be used
+        :type framework_slug: str
+        :return: response from endpoint
+        :rtype: Dict
         """
-        response = self.api_client.remove_supplier_declaration(supplier_id, self.framework_slug, 'user')
-        if response.status_code == 200:
-            return True
+        if self.dry_run:
+            self.logger.info("Would remove declaration from suppler with id %s that applied to framework %s",
+                             supplier_id, framework_slug
+                             )
         else:
-            return False
+            return self.api_client.remove_supplier_declaration(supplier_id, framework_slug, 'user')
 
+    def remove_declaration_from_failed_applicants(self):
+        """
+        This method gets a list of failed applicants and then calls the remove_declaration function for each one. It
+        returns None.
+        :return: None
+        :rtype: None
+        """
+        failed_supplier_ids = self.suppliers_application_failed_to_framework()
+        for supplier_id in failed_supplier_ids:
+            self.remove_declaration(supplier_id)
 
-def main():
-    pass
+    def _frameworks_older_than_date(self, date_closed):
+        """
+        Returns a list of the slugs of frameworks that closed prior to the date passed to the method
+        :param date_closed: the method will return frameworks that closed prior to this value
+        :type date_closed: date
+        :return: list of framework slugs
+        :rtype: List[str]
+        """
+        all_frameworks = self.api_client.find_frameworks()
+        return [framework['slug'] for framework in all_frameworks if framework['frameworkExpiresAtUTC'] <= date_closed]
 
-
-if __name__ == '__main__':
-    main()
+    def remove_declaration_from_suppliers(self, date_from=None):
+        """
+        Gets a list of all suppliers on all frameworks older than 'date_from' and removes their declarations
+        :param date_from: the date before which declarations should be removed
+        :type date_from: datetime
+        :return: None
+        :rtype: None
+        """
+        if not date_from:
+            date_from = date.today() - timedelta(365 * 3)
+        old_frameworks = self._frameworks_older_than_date(date_from)
+        for framework in old_frameworks:
+            suppliers_with_declarations_to_clear = self.api_client.find_framework_suppliers(
+                framework_slug=framework
+            )['supplierFrameworks']
+            for supplier in suppliers_with_declarations_to_clear:
+                self.remove_declaration(supplier['supplierId'], framework_slug=framework)
