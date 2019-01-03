f62adfec99557f6dd907c5e42289be8df8c2433a
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2019-01-03 15:38:55 +0000

    Moves AuditEvent user out to script entry point
    This commit makes the SupplierFrameworkDeclarations class more generic by removing the harcoded user. Instead, the user is passed through from the entry-point, where the script is first called.

--- dmscripts/data_retention_remove_supplier_declarations.py
+++ dmscripts/data_retention_remove_supplier_declarations.py
@@ -1,19 +1,21 @@
 from dmscripts.helpers.supplier_data_helpers import SupplierFrameworkDeclarations
 
 
-def remove_supplier_data(data_api_client, logger, dry_run):
+def remove_supplier_data(data_api_client, logger, dry_run, user):
     supplier_frameworks = SupplierFrameworkDeclarations(
         api_client=data_api_client,
         logger=logger,
-        dry_run=dry_run
+        dry_run=dry_run,
+        user=user
     )
     supplier_frameworks.remove_supplier_declaration_for_expired_frameworks()
 
 
-def remove_unsuccessful_supplier_declarations(data_api_client, logger, dry_run, framework_slug):
+def remove_unsuccessful_supplier_declarations(data_api_client, logger, dry_run, framework_slug, user):
     supplier_frameworks_methods = SupplierFrameworkDeclarations(
         api_client=data_api_client,
         logger=logger,
-        dry_run=dry_run
+        dry_run=dry_run,
+        user=user
     )
     supplier_frameworks_methods.remove_declaration_from_failed_applicants(framework_slug)
