aee9c95a9b4a373238b387b3acf9f922d393cf27
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-04-03 08:30:35 +0100

    Increase visibility of 'no data found' log messages

--- dmscripts/generate_supplier_user_csv.py
+++ dmscripts/generate_supplier_user_csv.py
@@ -9,7 +9,7 @@ def generate_supplier_csv(framework_slug, data_api_client, logger):
 
     supplier_rows = data_api_client.export_suppliers(framework_slug).get('suppliers', [])
     if not supplier_rows:
-        logger.info('No supplier data found for framework {}'.format(framework_slug))
+        logger.warn('No supplier data found for framework {}'.format(framework_slug))
         return [], [], None
 
     supplier_and_framework_headers = [
@@ -75,7 +75,7 @@ def generate_user_csv(framework_slug, data_api_client, user_research_opted_in, l
     ]
     user_rows = data_api_client.export_users(framework_slug).get('users', [])
     if not user_rows:
-        logger.info('No user data found for framework {}'.format(framework_slug))
+        logger.warn('No user data found for framework {}'.format(framework_slug))
         return [], [], None
 
     if user_research_opted_in:
