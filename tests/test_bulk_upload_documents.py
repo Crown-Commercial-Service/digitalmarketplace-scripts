5ccf3c51522265b8147918149270336c6dacbab6
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-04-26 11:39:13 +0100

    Organise tests into classes

--- tests/test_bulk_upload_documents.py
+++ tests/test_bulk_upload_documents.py
@@ -36,99 +36,100 @@ class TestGetBucketName:
         assert get_bucket_name('local', 'bananas') is None
 
 
-def test_get_all_files_of_type_for_flat_folder():
-    temp_folder_path = tempfile.mkdtemp()
-    pdf1 = open(os.path.join(temp_folder_path, 'test1.pdf'), 'w+')
-    pdf2 = open(os.path.join(temp_folder_path, 'test2.pdf'), 'w+')
-    assert len(list(get_all_files_of_type(temp_folder_path, 'pdf'))) == 2
-    pdf1.close()
-    pdf2.close()
-    shutil.rmtree(temp_folder_path)
+class TestGetAllFilesOfType:
+
+    def test_get_all_files_of_type_for_flat_folder(self):
+        temp_folder_path = tempfile.mkdtemp()
+        pdf1 = open(os.path.join(temp_folder_path, 'test1.pdf'), 'w+')
+        pdf2 = open(os.path.join(temp_folder_path, 'test2.pdf'), 'w+')
+        assert len(list(get_all_files_of_type(temp_folder_path, 'pdf'))) == 2
+        pdf1.close()
+        pdf2.close()
+        shutil.rmtree(temp_folder_path)
+
+    def test_get_all_files_of_type_for_nested_folder(self):
+        temp_folder_path = tempfile.mkdtemp()
+        pdf1 = open(os.path.join(temp_folder_path, 'test1.pdf'), 'w+')
+        nested_temp_folder_path = tempfile.mkdtemp(dir=temp_folder_path)
+        pdf2 = open(os.path.join(nested_temp_folder_path, 'test2.pdf'), 'w+')
+        assert len(list(get_all_files_of_type(temp_folder_path, 'pdf'))) == 2
+        pdf1.close()
+        pdf2.close()
+        shutil.rmtree(temp_folder_path)
 
 
-def test_get_all_files_of_type_for_nested_folder():
-    temp_folder_path = tempfile.mkdtemp()
-    pdf1 = open(os.path.join(temp_folder_path, 'test1.pdf'), 'w+')
-    nested_temp_folder_path = tempfile.mkdtemp(dir=temp_folder_path)
-    pdf2 = open(os.path.join(nested_temp_folder_path, 'test2.pdf'), 'w+')
-    assert len(list(get_all_files_of_type(temp_folder_path, 'pdf'))) == 2
-    pdf1.close()
-    pdf2.close()
-    shutil.rmtree(temp_folder_path)
+class TestGetSupplierIDFromFrameworkFilePath:
+
+    @pytest.mark.parametrize(
+        'path',
+        [
+            '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/ICNT_Consulting_Ltd-584425-signed-framework-agreement.pdf'  # noqa
+            '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/ICNT_Consulting_Ltd-584425_signed_framework_agreement.pdf'  # noqa
+            '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/584425-result-letter.pdf'
+        ]
+    )
+    def test_get_supplier_id_from_framework_file_path_for_file(self, path):
+        assert get_supplier_id_from_framework_file_path(path) == '584425'
 
 
-def test_get_supplier_id_from_framework_file_path_for_file_with_dashes():
-    path = '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/ICNT_Consulting_Ltd-584425-signed-framework-agreement.pdf'  # noqa
-    assert get_supplier_id_from_framework_file_path(path) == '584425'
+class TestGetDocumentNameFromFilePath:
+
+    def test_get_document_name_from_file_path(self):
+        path = '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/584425-result-letter.pdf'
+        assert get_document_name_from_file_path(path) == 'result-letter.pdf'
 
 
-def test_get_supplier_id_from_framework_file_path_for_file_with_underscores():
-    path = '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/ICNT_Consulting_Ltd-584425_signed_framework_agreement.pdf'  # noqa
-    assert get_supplier_id_from_framework_file_path(path) == '584425'
+class TestGetSupplierNameDictFromTSV:
+
+    @mock.patch('dmscripts.bulk_upload_documents.csv')
+    def test_get_supplier_name_dict_from_tsv(self, csv):
+        csv.reader.return_value = [('584425', 'ICNT_Consulting_Ltd'), ('35435', 'Something')]
+        with mock.patch.object(builtins, 'open', mock.mock_open()):
+            assert get_supplier_name_dict_from_tsv('test_csv.pdf') == {'35435': 'Something', '584425': 'ICNT_Consulting_Ltd'}  # noqa
 
 
-def test_get_supplier_id_from_framework_file_path_for_file_starting_with_supplier_id():
-    path = '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/584425-result-letter.pdf'  # noqa
-    assert get_supplier_id_from_framework_file_path(path) == '584425'
+class TestUploadFile:
 
+    def test_upload_file(self):
+        file_path = 'The_Business_Software_Centre-92877-signed-framework-agreement.pdf'
+        bucket = mock.Mock()
+        with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
+            upload_file(bucket, False, file_path, 'g-cloud-7', 'agreements', 'countersigned_agreement', 'pdf')
+            assert bucket.save.call_count == 1
+            bucket.save.assert_called_with(
+                'g-cloud-7/agreements/92877/92877-countersigned_agreement.pdf',
+                mock.ANY,
+                acl='bucket-owner-full-control',
+                download_filename=None)
 
-def test_get_document_name_from_file_path():
-    path = '../../Downloads/Completed Frameworks BATCH2 SHARED WITH GDS/584425-result-letter.pdf'  # noqa
-    assert get_document_name_from_file_path(path) == 'result-letter.pdf'
+    def test_upload_file_dry_run_doesnt_upload(self):
+        file_path = 'The_Business_Software_Centre-92877-signed-framework-agreement.pdf'
+        bucket = mock.Mock()
+        with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
+            upload_file(bucket, True, file_path, 'g-cloud-7', 'agreements', 'countersigned_agreement', 'pdf')
+            assert bucket.save.call_count == 0
 
+    def test_upload_file_without_document_category(self):
+        file_path = '/92877-framework-agreement.pdf'
+        bucket = mock.Mock()
+        with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
+            upload_file(bucket, False, file_path, 'g-cloud-7', 'agreements')
+            assert bucket.save.call_count == 1
+            bucket.save.assert_called_with(
+                'g-cloud-7/agreements/92877/92877-framework-agreement.pdf',
+                mock.ANY,
+                acl='bucket-owner-full-control',
+                download_filename=None)
 
-@mock.patch('dmscripts.bulk_upload_documents.csv')
-def test_get_supplier_name_dict_from_tsv(csv):
-    csv.reader.return_value = [('584425', 'ICNT_Consulting_Ltd'), ('35435', 'Something')]
-    with mock.patch.object(builtins, 'open', mock.mock_open()):
-        assert get_supplier_name_dict_from_tsv('test_csv.pdf') == {'35435': 'Something', '584425': 'ICNT_Consulting_Ltd'}  # noqa
-
-
-def test_upload_file():
-    file_path = 'The_Business_Software_Centre-92877-signed-framework-agreement.pdf'
-    bucket = mock.Mock()
-    with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
-        upload_file(bucket, False, file_path, 'g-cloud-7', 'agreements',
-                    'countersigned_agreement', 'pdf')
-        assert bucket.save.call_count == 1
-        bucket.save.assert_called_with(
-            'g-cloud-7/agreements/92877/92877-countersigned_agreement.pdf',
-            mock.ANY,
-            acl='bucket-owner-full-control',
-            download_filename=None)
-
-
-def test_upload_file_dry_run_doesnt_upload():
-    file_path = 'The_Business_Software_Centre-92877-signed-framework-agreement.pdf'
-    bucket = mock.Mock()
-    with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
-        upload_file(bucket, True, file_path, 'g-cloud-7', 'agreements',
-                    'countersigned_agreement', 'pdf')
-        assert bucket.save.call_count == 0
-
-
-def test_upload_file_without_document_category():
-    file_path = '/92877-framework-agreement.pdf'
-    bucket = mock.Mock()
-    with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
-        upload_file(bucket, False, file_path, 'g-cloud-7', 'agreements')
-        assert bucket.save.call_count == 1
-        bucket.save.assert_called_with(
-            'g-cloud-7/agreements/92877/92877-framework-agreement.pdf',
-            mock.ANY,
-            acl='bucket-owner-full-control',
-            download_filename=None)
-
-
-def test_upload_file_with_supplier_name_dictionary():
-    file_path = '/35435-framework-agreement.pdf'
-    supplier_name_dictionary = {'35435': 'Something', '584425': 'ICNT_Consulting_Ltd'}
-    bucket = mock.Mock()
-    with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
-        upload_file(bucket, False, file_path, 'g-cloud-7', 'agreements', supplier_name_dict=supplier_name_dictionary)  # noqa
-        assert bucket.save.call_count == 1
-        bucket.save.assert_called_with(
-            'g-cloud-7/agreements/35435/35435-framework-agreement.pdf',
-            mock.ANY,
-            acl='bucket-owner-full-control',
-            download_filename='Something-35435-framework-agreement.pdf')
+    def test_upload_file_with_supplier_name_dictionary(self):
+        file_path = '/35435-framework-agreement.pdf'
+        supplier_name_dictionary = {'35435': 'Something', '584425': 'ICNT_Consulting_Ltd'}
+        bucket = mock.Mock()
+        with mock.patch.object(builtins, 'open', mock.mock_open(read_data='foo')):
+            upload_file(bucket, False, file_path, 'g-cloud-7', 'agreements', supplier_name_dict=supplier_name_dictionary)  # noqa
+            assert bucket.save.call_count == 1
+            bucket.save.assert_called_with(
+                'g-cloud-7/agreements/35435/35435-framework-agreement.pdf',
+                mock.ANY,
+                acl='bucket-owner-full-control',
+                download_filename='Something-35435-framework-agreement.pdf')
