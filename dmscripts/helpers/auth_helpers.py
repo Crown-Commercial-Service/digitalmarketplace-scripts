1024c637c342e39a0d62311afe5d03b58f4aa28b
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-05-08 16:47:32 +0100

    Helper to retrieve Jenkins vars from credentials
    Refactors the sops decryption for testability

--- dmscripts/helpers/auth_helpers.py
+++ dmscripts/helpers/auth_helpers.py
@@ -5,6 +5,15 @@ import yaml
 DEV_ALIASES = ('dev', 'development', 'local')
 
 
+def _decrypt_yaml_file_with_sops(credentials_repo_path, credentials_filename):
+    creds_output = subprocess.check_output([
+        "{}/sops-wrapper".format(credentials_repo_path),
+        "-d",
+        "{}/{}".format(credentials_repo_path, credentials_filename)
+    ])
+    return yaml.safe_load(creds_output)
+
+
 def get_auth_token(api, stage):
     if stage.lower() in DEV_ALIASES:
         return 'myToken'
@@ -17,12 +26,43 @@ def get_auth_token(api, stage):
     if not auth_token:
         DM_CREDENTIALS_REPO = os.environ.get('DM_CREDENTIALS_REPO', '../digitalmarketplace-credentials')
         token_prefix = 'J' if subprocess.check_output(["whoami"]) == 'jenkins' else 'D'
-        creds = subprocess.check_output([
-            "{}/sops-wrapper".format(DM_CREDENTIALS_REPO),
-            "-d",
-            "{}/vars/{}.yaml".format(DM_CREDENTIALS_REPO, stage)
-        ])
-        auth_tokens = yaml.safe_load(creds)[api]['auth_tokens']
+        creds_json = _decrypt_yaml_file_with_sops(DM_CREDENTIALS_REPO, "vars/{}.yaml".format(stage))
+        auth_tokens = creds_json[api]['auth_tokens']
         auth_token = next(token for token in auth_tokens if token.startswith(token_prefix))
 
     return auth_token
+
+
+def _get_nested_value(creds_json, env_var_path_list):
+    # Handle nested values in the credentials json
+    if len(env_var_path_list) > 1:
+        return _get_nested_value(creds_json.get(env_var_path_list[0], {}), env_var_path_list[1:])
+    return creds_json.get(env_var_path_list[0])
+
+
+def get_jenkins_env_variable_from_credentials(env_var_dot_path, require_jenkins_user=True):
+    """
+    Scripts run on Jenkins can retrieve a variable from the Jenkins vars
+    env_var_dot_path: a dot separated string, e.g. "jenkins_env_variables.NOTIFY_API_TOKEN"
+    """
+    if require_jenkins_user and subprocess.check_output(["whoami"]) != 'jenkins':
+        raise ValueError("Only Jenkins user can retrieve this value")
+
+    DM_CREDENTIALS_REPO = os.environ.get('DM_CREDENTIALS_REPO', '../digitalmarketplace-credentials')
+    creds_json = _decrypt_yaml_file_with_sops(DM_CREDENTIALS_REPO, 'jenkins-vars/jenkins.yaml')
+
+    return _get_nested_value(creds_json, env_var_dot_path.split('.'))
+
+
+def get_jenkins_env_variable(jenkins_var_name):
+    # Check the local env first, otherwise decrypt from credentials
+    value = os.environ.get(jenkins_var_name)
+
+    if not value:
+        # Only allow Jenkins users to decrypt from credentials for now
+        value = get_jenkins_env_variable_from_credentials(
+            "jenkins_env_variables.{}".format(jenkins_var_name),
+            require_jenkins_user=True
+        )
+
+    return value
