79a7bc0a01ed1ddfd1fc5d92a5c349c943ec3983
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2020-03-11 10:59:50 +0000

    Add human-readable log output to Jenkins console

--- scripts/snapshot-framework-stats.py
+++ scripts/snapshot-framework-stats.py
@@ -26,6 +26,33 @@ def get_stats(data_client, framework_slug):
     return data_client.get_framework_stats(framework_slug)
 
 
+def log_human_readable_stats(stats):
+    total_applications = 0
+    made_declaration = 0
+    added_services = 0
+    completed = 0
+    started = 0
+
+    for stat in stats['interested_suppliers']:
+        total_applications += stat['count']
+        if stat['has_completed_services']:
+            if stat['declaration_status'] == 'complete':
+                completed += stat['count']
+            elif stat['declaration_status'] == 'started':
+                added_services += stat['count']
+        else:
+            if stat['declaration_status'] == 'complete':
+                made_declaration += stat['count']
+            else:
+                started += stat['count']
+
+    logger.info(f"Started: {started}")
+    logger.info(f"Made declaration: {made_declaration}")
+    logger.info(f"Added services: {added_services}")
+    logger.info(f"Completed: {completed}")
+    logger.info(f"Total applications: {total_applications}")
+
+
 def snapshot_framework_stats(api_endpoint, api_token, framework_slug):
     data_client = dmapiclient.DataAPIClient(api_endpoint, api_token)
 
@@ -36,6 +63,7 @@ def snapshot_framework_stats(api_endpoint, api_token, framework_slug):
         object_type='frameworks',
         object_id=framework_slug
     )
+    log_human_readable_stats(stats)
 
     logger.info("Framework stats snapshot saved")
 
