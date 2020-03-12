d5137971fb527b5faf933bb3ecf88f8f3842533c
benvand <benjaminfvandersteen@gmail.com>
2020-03-12 17:15:12 +0000

    Make application stats script output to file

--- scripts/snapshot-framework-stats.py
+++ scripts/snapshot-framework-stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Usage:
-    snapshot_framework_stats.py <framework_slug> <stage>
+    snapshot_framework_stats.py <stage> <framework_slug> <filename>
 
 Example:
-    ./snapshot_framework_stats.py g-cloud-7 dev
+    ./snapshot_framework_stats.py production g-cloud-12 myfile.json
 """
 
 from docopt import docopt
+import json
 import logging
 import sys
 
@@ -22,59 +23,25 @@ logger = logging.getLogger('script')
 logging.basicConfig(level=logging.INFO)
 
 
-def get_stats(data_client, framework_slug):
-    return data_client.get_framework_stats(framework_slug)
-
-
-def log_human_readable_stats(stats):
-    total_applications = 0
-    made_declaration = 0
-    added_services = 0
-    completed = 0
-    started = 0
-
-    for stat in stats['interested_suppliers']:
-        total_applications += stat['count']
-        if stat['has_completed_services']:
-            if stat['declaration_status'] == 'complete':
-                completed += stat['count']
-            elif stat['declaration_status'] == 'started':
-                added_services += stat['count']
-        else:
-            if stat['declaration_status'] == 'complete':
-                made_declaration += stat['count']
-            else:
-                started += stat['count']
-
-    logger.info("*********** STATS ***********")
-    logger.info(f"Started: {started}")
-    logger.info(f"Made declaration: {made_declaration}")
-    logger.info(f"Added services: {added_services}")
-    logger.info(f"Completed: {completed}")
-    logger.info(f"Total applications: {total_applications}")
-    logger.info("*****************************")
-
-
-def snapshot_framework_stats(api_endpoint, api_token, framework_slug):
-    data_client = dmapiclient.DataAPIClient(api_endpoint, api_token)
-
-    stats = get_stats(data_client, framework_slug)
-    data_client.create_audit_event(
+def snapshot_framework_stats(client, framework_slug):
+    stats = client.get_framework_stats(framework_slug)
+    client.create_audit_event(
         AuditTypes.snapshot_framework_stats,
         data=stats,
         object_type='frameworks',
         object_id=framework_slug
     )
-    log_human_readable_stats(stats)
 
-    logger.info("Framework stats snapshot saved")
+    logger.info("Framework stats snapshot saved as audit event")
+    return stats
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-
-    snapshot_framework_stats(
+    client = dmapiclient.DataAPIClient(
         get_api_endpoint_from_stage(arguments['<stage>']),
         get_auth_token('api', arguments['<stage>']),
-        arguments['<framework_slug>'],
     )
+    stats = snapshot_framework_stats(client, arguments['<framework_slug>'])
+    with open(arguments['<filename>'], 'w') as outfile:
+        json.dump(stats, outfile)
