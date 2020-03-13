cb6b4f6e798a147a6c7e3959422070e85db1ba5b
benvand <benjaminfvandersteen@gmail.com>
2020-03-13 09:04:32 +0000

    Move script to framework-applications dir

--- scripts/framework-applications/snapshot-framework-stats.py
+++ scripts/framework-applications/snapshot-framework-stats.py
@@ -23,6 +23,35 @@ logger = logging.getLogger('script')
 logging.basicConfig(level=logging.INFO)
 
 
+def log_human_readable_stats(stats):
+    total_applications = 0
+    made_declaration = 0
+    added_services = 0
+    completed = 0
+    started = 0
+
+    for stat in stats['interested_suppliers']:
+        total_applications += stat['count']
+        if stat['has_completed_services']:
+            if stat['declaration_status'] == 'complete':
+                completed += stat['count']
+            elif stat['declaration_status'] == 'started':
+                added_services += stat['count']
+        else:
+            if stat['declaration_status'] == 'complete':
+                made_declaration += stat['count']
+            else:
+                started += stat['count']
+
+    logger.info("*********** STATS ***********")
+    logger.info(f"Started: {started}")
+    logger.info(f"Made declaration: {made_declaration}")
+    logger.info(f"Added services: {added_services}")
+    logger.info(f"Completed: {completed}")
+    logger.info(f"Total applications: {total_applications}")
+    logger.info("*****************************")
+
+
 def snapshot_framework_stats(client, framework_slug):
     stats = client.get_framework_stats(framework_slug)
     client.create_audit_event(
@@ -31,6 +60,7 @@ def snapshot_framework_stats(client, framework_slug):
         object_type='frameworks',
         object_id=framework_slug
     )
+    log_human_readable_stats(stats)
 
     logger.info("Framework stats snapshot saved as audit event")
     return stats
