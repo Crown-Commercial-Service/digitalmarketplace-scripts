83f559f72cc2d04ff2b07de022314e6d04638230
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-09-28 12:22:14 +0100

    Add unsuspend script

--- dmscripts/suspend_suppliers_without_agreements.py
+++ dmscripts/suspend_suppliers_without_agreements.py
@@ -1,5 +1,7 @@
 from itertools import chain
 
+from dmapiclient.audit import AuditTypes
+
 
 def get_all_email_addresses_for_supplier(client, supplier_framework):
     # Combine the framework application contact email with all the active user emails
@@ -61,3 +63,68 @@ def suspend_supplier_services(client, logger, framework_slug, supplier_id, frame
 
     # Return suspended service count (i.e. if > 0, some emails need to be sent)
     return suspended_service_count
+
+
+def unsuspend_supplier_services(client, logger, framework_slug, supplier_id, framework_info, dry_run):
+    """
+    The supplier ID list should have been flagged by CCS as requiring action, but double check that the supplier:
+      - has some services on the framework
+      - has not `agreementReturned: false`
+      - has not `agreementReturned: on-hold
+    :param client: API client instance
+    :param framework_info: JSON
+    :param dry_run: don't suspend if True
+    :return: suspended_service_count
+    :rtype: int
+    """
+    unsuspended_service_count = 0
+
+    new_service_status, old_service_status = 'published', 'disabled'
+
+    if not framework_info['frameworkInterest']['onFramework']:
+        logger.error(f'Supplier {supplier_id} is not on the framework.')
+        return unsuspended_service_count
+    if framework_info['frameworkInterest']['agreementReturned'] is None:
+        logger.error(f'Supplier {supplier_id} has not returned their framework agreement.')
+        return unsuspended_service_count
+    if framework_info['frameworkInterest']['agreementStatus'] == 'on-hold':
+        logger.error(f"Supplier {supplier_id}'s framework agreement is on hold.")
+        return unsuspended_service_count
+
+    # Find the supplier's services which were suspended by script
+    suspended_services_on_framework = set(
+        service["id"] for service in
+        client.find_services(
+            supplier_id=supplier_id, framework=framework_slug, status=old_service_status
+        )["services"]
+    )
+
+    if not suspended_services_on_framework:
+        logger.error(f'Supplier {supplier_id} has no {old_service_status} services on the framework.')
+        return suspended_service_count
+
+    services_suspended_by_script = set(
+        event["data"]["serviceId"]
+        for event in
+        client.find_audit_events_iter(
+            audit_type=AuditTypes.update_service_status,
+            data_supplier_id=supplier_id,
+            user="Suspend services script",
+        )
+    )
+
+    service_ids = suspended_services_on_framework & services_suspended_by_script
+
+    # Suspend all services for each supplier (the API will de-index the services from search results)
+    logger.info(
+        f"Setting {len(service_ids)} services to '{new_service_status}' for supplier {supplier_id}."
+    )
+    for service_id in service_ids:
+        if dry_run:
+            logger.info(f"[DRY RUN] Would suspend service {service_id} for supplier {supplier_id}")
+        else:
+            client.update_service_status(service_id, new_service_status, "Suspend services script")
+        unsuspended_service_count += 1
+
+    # Return suspended service count (i.e. if > 0, some emails need to be sent)
+    return unsuspended_service_count
---
+++ scripts/framework-applications/unsuspend-suppliers-who-have-signed-since-being-suspended.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+After a framework goes live, suppliers have a short period (usually 2 weeks) within which they must return their
+signed framework agreement. If they have not returned their agreement, it cannot be countersigned by CCS and the
+supplier will be unable to sell their services. We therefore temporarily suspend the supplier's services
+so that buyers won't see them in search results (G-Cloud) or the supplier cannot apply to an opportunity (DOS).
+If they later do sign their framework agreement we can then un-suspend them.
+
+This script carries out the same action that a CCS Category user could perform in the admin, but in bulk.
+In the past CCS have been responsible for unsuspending suppliers, however this script can be used to find and
+unsuspend all suppliers who were suspended if we wish to do so for whatever reason.
+
+Usage:
+    scripts/framework-applications/unsuspend-suppliers-who-have-signed-since-being-suspended.py
+        [-v...] [options]
+        <stage> <framework> <output_dir>
+        [--supplier-id=<id>... | --supplier-ids-from=<file>]
+    scripts/framework-applications/unsuspend-suppliers-who-have-signed-since-being-suspended.py (-h | --help)
+
+Options:
+    <stage>                     Environment to run script against.
+    <framework>                 Slug of framework to generate agreements for.
+    <output_dir>                Output folder for list of email addresses to send notifications to.
+
+    --supplier-id=<id>          ID of supplier to unsuspend.
+    --supplier-ids-from=<file>  Path to file containing supplier IDs, one per line.
+
+    --suspended-user=<user>     When searching for suppliers to unsuspend this argument specifies the user (or script)
+                                who previously suspended the suppliers [default: Suspend services script].
+    --suspended-date=<date>     The date when suppliers where previously unsuspended.
+
+    -h, --help                  Show this help message
+
+    -n, --dry-run               Run script without making changes.
+    -v, --verbose               Show debug log messages.
+
+    If neither `--supplier-ids-from` or `--supplier-id` are provided then
+    all suppliers who without framework agreements will be suspended.
+"""
+import csv
+import sys
+import pathlib
+
+from docopt import docopt
+
+sys.path.insert(0, '.')
+
+from dmscripts.helpers.auth_helpers import get_auth_token
+from dmscripts.helpers.logging_helpers import (
+    configure_logger,
+    logging,
+)
+from dmscripts.helpers.framework_helpers import find_suppliers_without_agreements
+from dmscripts.helpers.supplier_data_helpers import get_supplier_ids_from_args
+
+from dmapiclient import DataAPIClient
+from dmapiclient.audit import AuditTypes
+from dmutils.env_helpers import get_api_endpoint_from_stage
+from dmscripts.suspend_suppliers_without_agreements import (
+    unsuspend_supplier_services, get_all_email_addresses_for_supplier
+)
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+
+    stage = args["<stage>"]
+    framework_slug = args["<framework>"]
+
+    dry_run = args["--dry-run"]
+    verbose = args["--verbose"]
+    output_dir = pathlib.Path(args["<output_dir>"])
+    FILENAME = f'{framework_slug}-unsuspended-suppliers.csv'
+
+    logger = configure_logger({
+        "dmapiclient.base": logging.WARNING,
+        "framework_helpers": logging.DEBUG if verbose >= 2 else logging.WARNING,
+        "script": logging.DEBUG if verbose else logging.INFO,
+    })
+
+    client = DataAPIClient(
+        get_api_endpoint_from_stage(args["<stage>"]),
+        get_auth_token("api", args["<stage>"]),
+    )
+
+    framework = client.get_framework(framework_slug)["frameworks"]
+    # Check that the framework is in live or standstill
+    if framework['status'] not in ['live', 'standstill']:
+        logger.error(f"Cannot suspend services for '{framework_slug}' with status {framework['status']}")
+        exit(1)
+
+    supplier_ids = get_supplier_ids_from_args(args)
+    if not supplier_ids:
+        # search for supplier who were suspended by --suspended-user
+        find_audit_events = {
+            "audit_date": args.get("--suspended-date"),
+            "audit_type": AuditTypes.update_service_status,
+            "user": args["--suspended-user"],
+        }
+
+        logger.debug(
+            "searching for audit events with find_audit_events("
+            "audit_date={audit_date!r}, audit_type={audit_type}, user={user!r})".format(**find_audit_events)
+        )
+
+        previously_suspended_suppliers = set(event["data"]["supplierId"] for event in client.find_audit_events_iter(**find_audit_events))
+
+    logger.info(f"going to try and un-suspend {len(supplier_ids)} suppliers")
+
+    with open(output_dir / FILENAME, 'w') as csvfile:
+        csv_headers = ['Supplier email', 'Supplier ID', "No. of services unsuspended"]
+
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(csv_headers)
+
+        for supplier_id in supplier_ids:
+            framework_info = client.get_supplier_framework_info(supplier_id, framework_slug)
+
+            if framework_info["frameworkInterest"]["agreementId"] is None:
+                logger.warn(f"supplier {supplier_id} does not have a framework agreement, will not un-suspend")
+                continue
+
+            # Do the un-suspending
+            unsuspended_service_count = unsuspend_supplier_services(
+                client, logger, framework_slug, supplier_id, framework_info, dry_run
+            )
+
+            if unsuspended_service_count == 0:
+                # We should have logged why already
+                continue
+
+            # Compile a list of email addresses for the supplier (to be sent via Notify) and add to the CSV
+            for supplier_email in get_all_email_addresses_for_supplier(client, framework_info):
+                writer.writerow([supplier_email, supplier_id, unsuspended_service_count])
