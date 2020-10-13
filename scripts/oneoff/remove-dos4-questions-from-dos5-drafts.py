3b05c9152d39a56be072bb769bef47d7130f38bb
Alex Smith <alex.smith@digital.cabinet-office.gov.uk>
2020-10-13 17:01:04 +0100

    Remove deleted dos4 answers

--- scripts/oneoff/remove-dos4-questions-from-dos5-drafts.py
+++ scripts/oneoff/remove-dos4-questions-from-dos5-drafts.py
@@ -48,13 +48,50 @@ def draft_service_contains_dos4_answer(draft: dict) -> bool:
         "designerAccessibleApplications"
     }
 
-    return len(invalid_answers.union(set(draft["services"].keys()))) > 0
+    return len(invalid_answers.union(set(draft.keys()))) > 0 and draft['status'] == 'not-submitted'
+
+
+def remove_dos4_answers(api_client: DataAPIClient, draft: dict, developer_email: str) -> dict:
+    return api_client.update_draft_service(
+        draft['id'],
+        {
+            "agileCoachPriceMin": None,
+            "businessAnalystPriceMin": None,
+            "communicationsManagerPriceMin": None,
+            "contentDesignerPriceMin": None,
+            "dataArchitectPriceMin": None,
+            "dataEngineerPriceMin": None,
+            "dataScientistPriceMin": None,
+            "deliveryManagerPriceMin": None,
+            "designerPriceMin": None,
+            "developerPriceMin": None,
+            "performanceAnalystPriceMin": None,
+            "portfolioManagerPriceMin": None,
+            "productManagerPriceMin": None,
+            "programmeManagerPriceMin": None,
+            "qualityAssurancePriceMin": None,
+            "securityConsultantPriceMin": None,
+            "serviceManagerPriceMin": None,
+            "technicalArchitectPriceMin": None,
+            "userResearcherPriceMin": None,
+            "webOperationsPriceMin": None,
+            "developerAccessibleApplications": None,
+            "contentDesignerAccessibleApplications": None,
+            "qualityAssuranceAccessibleApplications": None,
+            "technicalArchitectAccessibleApplications": None,
+            "webOperationsAccessibleApplications": None,
+            "serviceManagerAccessibleApplications": None,
+            "designerAccessibleApplications": None
+        },
+        developer_email
+    )
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("stage", type=str)
+    parser.add_argument("developer_email", type=str)
 
     args = parser.parse_args()
 
@@ -62,6 +99,12 @@ if __name__ == '__main__':
         get_api_endpoint_from_stage(args.stage),
         get_auth_token('api', args.stage)
     )
-
-    affected_drafts = get_affected_drafts_services(data)
-    print(affected_drafts)
+    
+    updated_drafts = [
+        remove_dos4_answers(data, draft, args.developer_email) for draft
+        in get_affected_drafts_services(data)
+    ]
+    
+    for draft in updated_drafts:
+        print(f"Supplier id: {draft['supplierId']}")
+        print(f"Draft id: {draft['id']}")
