f57976349b0bdc4db3c970dc1cb453f95a6cde09
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2020-10-29 11:27:12 +0000

    Create script to create test suppliers/users
    We will use these freshly created suppliers and associated users for our load testing

---
+++ scripts/create_test_users.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from dmapiclient import DataAPIClient
+
+sys.path.insert(0, '.')
+
+from dmscripts.helpers.auth_helpers import get_auth_token
+from dmscripts.generate_supplier_user_csv import generate_csv_and_upload_to_s3
+from dmscripts.helpers import logging_helpers
+from dmscripts.helpers.logging_helpers import logging
+from dmscripts.helpers.s3_helpers import get_bucket_name
+from dmutils.env_helpers import get_api_endpoint_from_stage
+
+stage = 'development'
+data_api_client = DataAPIClient(get_api_endpoint_from_stage(stage), get_auth_token('api', stage))
+
+
+def ensure_supplier_exists(data_api_client, duns_number):
+    try:
+        [supplier] = data_api_client.find_suppliers(duns_number=duns_number)['suppliers']
+    except ValueError:
+        print(f"Creating user with DUNS {duns_number}")
+        data_api_client.create_supplier(
+            {'companiesHouseNumber': '12345678',
+             'contactInformation': [{'address1': 'Supplier Contact Address 1',
+                                     'city': 'Supplier Contact City',
+                                     'contactName': 'Supplier Contact',
+                                     'email': 'simulate-delivered@notifications.service.gov.uk',
+                                     'personalDataRemoved': False,
+                                     'phoneNumber': '555123456788',
+                                     'postcode': 'AA11 1AA'}],
+             'description': 'Test supplier',
+             'dunsNumber': str(duns_number),
+             'name': 'Test Supplier',
+             }
+        )
+        [supplier] = data_api_client.find_suppliers(duns_number=duns_number)['suppliers']
+
+    data_api_client.update_supplier(
+        supplier['id'],
+        {
+            'companyDetailsConfirmed': True,
+            'organisationSize': 'small',
+            'otherCompanyRegistrationNumber': 'Test',
+            'registeredName': 'Test Supplier LIMITED',
+            'registrationCountry': 'country:GB',
+            'tradingStatus': 'limited company (LTD)',
+            'vatNumber': '123456788'
+        },
+        user='benjamin.gill'
+    )
+
+    return data_api_client.find_suppliers(duns_number=duns_number)['suppliers'][0]
+
+
+def ensure_user_exists_for_supplier(data_api_client, supplier_id):
+    try:
+        user = next(data_api_client.find_users_iter(supplier_id=supplier_id))
+    except StopIteration:
+        data_api_client.create_user({
+            "emailAddress": f"{supplier_id}@example.com",
+            "name": "Test",
+            "password": "Password1234",
+            'role': 'supplier',
+            'phoneNumber': '555123456788',
+            'supplierId': supplier_id,
+        })
+        user = next(data_api_client.find_users_iter(supplier_id=supplier_id))
+
+    return user
+
+
+for duns_number in range(123456787, 123456790):
+    supplier = ensure_supplier_exists(data_api_client, duns_number)
+    print(f"Supplier: {supplier['id']}")
+    user = ensure_user_exists_for_supplier(data_api_client, supplier['id'])
+    print(f"User: {user['emailAddress']}")
