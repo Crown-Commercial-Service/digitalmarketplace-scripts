01f14d160a96c574a2ee762d37cb8eb7500cadc0
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2020-10-29 14:48:58 +0000

    Create test users with fixed email addresses
    Supplier ID is generated at the point the supplier is created, so will vary from environment to environment. Instead, use the DUNS number, which we control.

--- scripts/create_test_users.py
+++ scripts/create_test_users.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-
-import os
+"""
+A script to create test suppliers and users for performance testing.
+"""
 import sys
 
 from dmapiclient import DataAPIClient
 
 sys.path.insert(0, '.')
-
 from dmscripts.helpers.auth_helpers import get_auth_token
-from dmscripts.generate_supplier_user_csv import generate_csv_and_upload_to_s3
-from dmscripts.helpers import logging_helpers
-from dmscripts.helpers.logging_helpers import logging
-from dmscripts.helpers.s3_helpers import get_bucket_name
 from dmutils.env_helpers import get_api_endpoint_from_stage
+
+PASSWORD = 'Password1234'
 
 stage = 'development'
 data_api_client = DataAPIClient(get_api_endpoint_from_stage(stage), get_auth_token('api', stage))
@@ -56,25 +54,25 @@ def ensure_supplier_exists(data_api_client, duns_number):
     return data_api_client.find_suppliers(duns_number=duns_number)['suppliers'][0]
 
 
-def ensure_user_exists_for_supplier(data_api_client, supplier_id):
-    try:
-        user = next(data_api_client.find_users_iter(supplier_id=supplier_id))
-    except StopIteration:
-        data_api_client.create_user({
-            "emailAddress": f"{supplier_id}@example.com",
-            "name": "Test",
-            "password": "Password1234",
-            'role': 'supplier',
-            'phoneNumber': '555123456788',
-            'supplierId': supplier_id,
-        })
-        user = next(data_api_client.find_users_iter(supplier_id=supplier_id))
+def ensure_user_exists_for_supplier(data_api_client, supplier_id, duns_number):
+    email_address = f"supplier{duns_number}@example.com"
 
-    return user
+    user = data_api_client.get_user(email_address=email_address)
+    if user:
+        return user['users']
+
+    data_api_client.create_user({
+        "emailAddress": email_address,
+        "name": "Test",
+        "password": PASSWORD,
+        'role': 'supplier',
+        'phoneNumber': '555123456788',
+        'supplierId': supplier_id,
+    })
+    return data_api_client.get_user(email_address=email_address)['users']
 
 
 for duns_number in range(123456787, 123456790):
     supplier = ensure_supplier_exists(data_api_client, duns_number)
-    print(f"Supplier: {supplier['id']}")
-    user = ensure_user_exists_for_supplier(data_api_client, supplier['id'])
-    print(f"User: {user['emailAddress']}")
+    user = ensure_user_exists_for_supplier(data_api_client, supplier['id'], duns_number)
+    print(f"{user['emailAddress']},{PASSWORD}")
