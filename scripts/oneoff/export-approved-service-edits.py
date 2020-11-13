24b73126b5e2422af5bf25d89a2c2ffcb88cb695
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-11-13 13:10:15 +0000

    Switch date of edit and date approved columns in csv
    CCS requested the columns be in a different order. They still want the
    data filtered by date approved however.

--- scripts/oneoff/export-approved-service-edits.py
+++ scripts/oneoff/export-approved-service-edits.py
@@ -125,8 +125,8 @@ if __name__ == "__main__":
 
     writer.writerow(
         [
-            "date of approval",
             "date of edit",
+            "date of approval",
             "approved by",
             "supplier name",
             "supplier ID",
@@ -137,8 +137,8 @@ if __name__ == "__main__":
     for e in audit_events:
         writer.writerow(
             [
-                e["acknowledgedAt"].strftime(ISO_FORMAT),
                 e["createdAt"],
+                e["acknowledgedAt"].strftime(ISO_FORMAT),
                 e["acknowledgedBy"],
                 e["data"].get("supplierName"),
                 e["data"].get("supplierId"),
