4ba9a5411680de6a1536dcd7cc2c32837a85b37a
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2020-12-10 15:29:24 +0000

    Get list of closed briefs more efficiently
    Instead of getting all the closed briefs and then filtering by date, use the options built into the API to do the filtering in the API. This gets us the list of briefs closed on that date much faster.
    
    Since we're no longer doing our own filtering, we no longer need the tests.

--- dmscripts/helpers/brief_data_helpers.py
+++ dmscripts/helpers/brief_data_helpers.py
@@ -1,9 +1,9 @@
-from datetime import datetime
-from dmutils.formats import DATETIME_FORMAT
+from datetime import datetime, date
+from typing import Union
+
+from dmutils.formats import DATE_FORMAT
 
 
-def get_briefs_closed_on_date(data_api_client, date_closed):
-    return [
-        brief for brief in data_api_client.find_briefs_iter(status='closed', with_users=True)
-        if datetime.strptime(brief['applicationsClosedAt'], DATETIME_FORMAT).date() == date_closed
-    ]
+def get_briefs_closed_on_date(data_api_client, date_closed: Union[datetime, date]):
+    return list(
+        data_api_client.find_briefs_iter(status='closed', with_users=True, closed_on=date_closed.strftime(DATE_FORMAT)))
