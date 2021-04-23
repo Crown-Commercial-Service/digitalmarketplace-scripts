eb64c0a0ab6f9b5faf356c6e3c8f0da5f39cf5ef
Gideon Goldberg <gideon.goldberg@digital.cabinet-office.gov.uk>
2021-04-23 09:46:39 +0100

    add supplier fields

--- scripts/oneoff/export_dos_supplier_subcontracting_responses.py
+++ scripts/oneoff/export_dos_supplier_subcontracting_responses.py
@@ -29,19 +29,22 @@ if __name__ == '__main__':
 
     data_api_client = DataAPIClient(get_api_endpoint_from_stage(stage), get_auth_token('api', stage))
 
-    def get_subcontracting_dict(supplier_id, declaration_fields):
-        id = {'supplier_id': supplier_id}
+    def get_subcontracting_dict(supplier_id, declaration_fields, supplier_fields):
+        id = {'supplierId': supplier_id}
         declaration = (data_api_client.get_supplier_framework_info(supplier_id, framework)['frameworkInterest']
                        .get('declaration'))
-        fields = {fieldname: declaration.get(fieldname) for fieldname in declaration_fields}
-        return {**id, **fields}
+        supplier = data_api_client.get_supplier(supplier_id)['suppliers']
+        declaration_fields = {fieldname: declaration.get(fieldname) for fieldname in declaration_fields}
+        supplier_fields = {fieldname: supplier.get(fieldname) for fieldname in supplier_fields}
+        return {**id, **declaration_fields, **supplier_fields}
 
     declaration_fields = ['subcontracting', 'subcontracting30DayPayments', 'subcontractingInvoicesPaid']
-    fieldnames = ['supplier_id'] + declaration_fields
+    supplier_fields = ['name', 'organisationSize', 'dunsNumber', 'companiesHouseNumber']
+    fieldnames = ['supplierId'] + declaration_fields + supplier_fields
 
     writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
     writer.writeheader()
-    suppliers = find_suppliers_on_framework(data_api_client, 'digital-outcomes-and-specialists-5')
+    suppliers = find_suppliers_on_framework(data_api_client, framework)
     for supplier in suppliers:
         supplier_id = supplier['supplierId']
-        writer.writerow(get_subcontracting_dict(supplier_id, declaration_fields))
+        writer.writerow(get_subcontracting_dict(supplier_id, declaration_fields, supplier_fields))
