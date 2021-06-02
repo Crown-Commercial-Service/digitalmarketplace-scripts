631ee6a869fcf2c2e07155a56e91a7f997a51d36
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-06-02 12:15:10 +0100

    Split up data retention helper function
    So that we can re-use it when we want to remove an individual user following a support request

--- dmscripts/data_retention_remove_user_data.py
+++ dmscripts/data_retention_remove_user_data.py
@@ -7,6 +7,55 @@ class MailchimpRemovalFailed(EnvironmentError):
     pass
 
 
+def remove_user_from_mailchimp(dm_mailchimp_client, logger, user, dry_run=True):
+    prefix = "[DRY RUN]: " if dry_run else ""
+
+    email_hash = dm_mailchimp_client.get_email_hash(user["emailAddress"])
+    logger.info(
+        "Checking mailing list membership for email with hash %s (user %s)",
+        email_hash,
+        user["id"],
+    )
+    mailing_lists = dm_mailchimp_client.get_lists_for_email(user["emailAddress"])
+    if mailing_lists:
+        for mailing_list in mailing_lists:
+            logger.warn(
+                "%sRemoving email with hash %s from list %s ('%s')",
+                prefix,
+                email_hash,
+                mailing_list["list_id"],
+                mailing_list["name"],
+            )
+            if not dry_run:
+                rm_result = dm_mailchimp_client.permanently_remove_email_from_list(
+                    email_address=user["emailAddress"],
+                    list_id=mailing_list["list_id"],
+                )
+                if not rm_result:
+                    raise MailchimpRemovalFailed(
+                        "Mailchimp failure trying to permanently_remove_email_from_list"
+                    )
+
+
+def remove_user_data(
+    data_api_client,
+    logger,
+    user,
+    dm_mailchimp_client=None,
+    dry_run=True,
+):
+    prefix = "[DRY RUN]: " if dry_run else ""
+
+    if dm_mailchimp_client is not None:
+        remove_user_from_mailchimp(dm_mailchimp_client, logger, user, dry_run)
+
+    logger.warn(f"{prefix}Removing personal data in API for user: {user['id']}")
+    if not dry_run:
+        data_api_client.remove_user_personal_data(
+            user["id"], "Data Retention Script {}".format(datetime.now().isoformat())
+        )
+
+
 def data_retention_remove_user_data(
     data_api_client,
     logger,
@@ -14,49 +63,10 @@ def data_retention_remove_user_data(
     dry_run=True,
 ):
     cutoff_date = datetime.now() - timedelta(days=365 * 3)
-    prefix = '[DRY RUN]: ' if dry_run else ''
 
     for user in data_api_client.find_users_iter(personal_data_removed=False):
-        last_logged_in_at = datetime.strptime(user['loggedInAt'], DATETIME_FORMAT)
+        last_logged_in_at = datetime.strptime(user["loggedInAt"], DATETIME_FORMAT)
         if last_logged_in_at < cutoff_date:
-            if dm_mailchimp_client is not None:
-                email_hash = dm_mailchimp_client.get_email_hash(user["emailAddress"])
-                logger.info(
-                    "Checking mailing list membership for email with hash %s (user %s)",
-                    email_hash,
-                    user["id"],
-                )
-                mailing_lists = dm_mailchimp_client.get_lists_for_email(user["emailAddress"])
-                if mailing_lists:
-                    for mailing_list in mailing_lists:
-                        logger.warn(
-                            f"%sRemoving email with hash %s from list %s ('%s')",
-                            prefix,
-                            email_hash,
-                            mailing_list["list_id"],
-                            mailing_list["name"],
-                        )
-                        if not dry_run:
-                            rm_result = dm_mailchimp_client.permanently_remove_email_from_list(
-                                email_address=user["emailAddress"],
-                                list_id=mailing_list["list_id"],
-                            )
-                            if not rm_result:
-                                raise MailchimpRemovalFailed(
-                                    "Mailchimp failure trying to permanently_remove_email_from_list"
-                                )
-                else:
-                    logger.info(
-                        "%s (user %s) not a member of any mailing lists",
-                        email_hash,
-                        user["id"],
-                    )
-
-            logger.warn(
-                f"{prefix}Removing personal data in API for user: {user['id']}"
+            remove_user_data(
+                data_api_client, logger, user, dm_mailchimp_client, dry_run
             )
-            if not dry_run:
-                data_api_client.remove_user_personal_data(
-                    user['id'],
-                    'Data Retention Script {}'.format(datetime.now().isoformat())
-                )
