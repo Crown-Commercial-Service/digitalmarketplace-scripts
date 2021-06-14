e31802dca570ca741e2aaa4e5ddbde5489288c41
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-06-14 13:21:50 +0100

    Document script dependency
    It needs you to have installed https://github.com/cli/cli

--- scripts/review-dependabot-prs.py
+++ scripts/review-dependabot-prs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """
 Show a human all Dependabot PRs that look ready to be merged. If the human accepts, approve and merge the PRs.
+
+Requires GitHub's CLI tool: https://github.com/cli/cli
 """
 
 import json
