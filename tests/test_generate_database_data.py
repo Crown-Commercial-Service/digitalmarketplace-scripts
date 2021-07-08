7a4392a486c991ced05d8c4315e55a0bc24f5167
Alex Smith <alex.smith@digital.cabinet-office.gov.uk>
2021-07-08 09:09:09 +0100

    Remove redundant `set()`

--- tests/test_generate_database_data.py
+++ tests/test_generate_database_data.py
@@ -14,7 +14,7 @@ class TestGenerateUser:
 
     def test_generate_user_has_correct_keys(self):
         buyer = generate_user(data=self.api_client, role="buyer")
-        assert set(buyer.keys()) == {"name", "email_address", "password", "role"}
+        assert buyer.keys() == {"name", "email_address", "password", "role"}
 
     @pytest.mark.parametrize("role", [role for role in USER_ROLES])
     def test_generate_user_allows_valid_roles(self, role):
