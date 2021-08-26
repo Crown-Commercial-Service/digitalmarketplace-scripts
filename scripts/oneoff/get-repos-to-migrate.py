bbee6b62440f2906c8d48683a82da15aed69573f
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-08-26 14:27:55 +0100

    Create script that will be used to get repos
    For migration to CCS

---
+++ scripts/oneoff/get-repos-to-migrate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+Shows all the repos controlled by the Digital Marketplace team, and whether they should be transferred to CCS
+"""
+
+if __name__ == "__main__":
+    pass
