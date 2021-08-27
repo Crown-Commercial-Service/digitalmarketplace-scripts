c126e728a113db59a253fac93dcedc3e9ed0637e
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-08-27 13:21:04 +0100

    Actually perform migrations
    Add a flag to allow the script to run the migration. Will need human confirmation for each repo migrated.

--- scripts/oneoff/get-repos-to-migrate.py
+++ scripts/oneoff/get-repos-to-migrate.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 """
 Shows all the repos controlled by the Digital Marketplace team, and whether they should be transferred to CCS
+
+Usage:
+    get-repos-to-migrate.py [--do-migration]
 """
 import json
 import subprocess
+
+from docopt import docopt
 
 DIGITAL_MARKETPLACE_TEAMS = {
     "digitalmarketplace",
@@ -48,21 +53,34 @@ def get_repos_for_team(team_name):
     return {repo["name"] for repo in json.loads(output.stdout)}
 
 
-def migrate_repo(repo_name, source_org, destination_org, new_team_id):
-    print(
-        [
-            "gh",
-            "api",
-            f"/repos/{source_org}/{repo_name}/transfer",
-            "--field",
-            f"new_owner={destination_org}",
-            "--field",
-            f"team_ids={new_team_id}",
-        ]
-    )
+def migrate_repo(
+    repo_name, source_org, destination_org, new_team_id, do_migration=False
+):
+    commands = [
+        "echo",
+        "gh",
+        "api",
+        f"/repos/{source_org}/{repo_name}/transfer",
+        "--field",
+        f"new_owner={destination_org}",
+        "--field",
+        f"team_ids={new_team_id}",
+    ]
+
+    if do_migration:
+        commands.pop(0)
+
+    subprocess.run(commands, check=True)
 
 
 if __name__ == "__main__":
+    arguments = docopt(__doc__)
+
+    if arguments.get("--do-migration"):
+        print("Confirm migration or quit")
+        input()
+        print("Confirmed. Performing migrations")
+
     admin_repos = get_repos_for_team("digitalmarketplace-admin") - REPOS_NOT_TO_MIGRATE
 
     non_admin_repos = (
@@ -84,10 +102,20 @@ if __name__ == "__main__":
         print(f"Migrate {repo}: https://github.com/alphagov/{repo}?")
         input()
         migrate_repo(
-            repo, "alphagov", CCS_ORGANISATION, CCS_DIGITALMARKETPLACE_ADMIN_TEAM_ID
+            repo,
+            "alphagov",
+            CCS_ORGANISATION,
+            CCS_DIGITALMARKETPLACE_ADMIN_TEAM_ID,
+            do_migration=arguments.get("--do-migration"),
         )
 
     for repo in non_admin_repos:
         print(f"Migrate {repo}: https://github.com/alphagov/{repo}?")
         input()
-        migrate_repo(repo, "alphagov", CCS_ORGANISATION, CCS_DIGITALMARKETPLACE_TEAM_ID)
+        migrate_repo(
+            repo,
+            "alphagov",
+            CCS_ORGANISATION,
+            CCS_DIGITALMARKETPLACE_TEAM_ID,
+            do_migration=arguments.get("--do-migration"),
+        )
