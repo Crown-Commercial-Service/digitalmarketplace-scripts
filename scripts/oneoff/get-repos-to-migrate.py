7fc5038e41fd12f83f77a071b0473db80222de26
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-08-27 12:50:09 +0100

    Exclude shared repos
    We (probably) don't want to take them with us.

--- scripts/oneoff/get-repos-to-migrate.py
+++ scripts/oneoff/get-repos-to-migrate.py
@@ -7,6 +7,21 @@ import subprocess
 
 DIGITAL_MARKETPLACE_TEAMS = {"digitalmarketplace", "digitalmarketplace-admin", "digitalmarketplace-readonly"}
 
+REPOS_NOT_TO_MIGRATE = {
+    # Repos shared with other teams where we're not admin, so shouldn't migrate them to CCS.
+    "gds-cli",
+    "gds-tech-learning-pathway",
+    "gds-tech-recruitment",
+    "gds-way",
+    "paas-roadmap",
+    "re-rotas",
+    "seal",
+    # TODO: work out whether to migrate these or not
+    "fourth-wall",  # {'team-payments': 'push', 'digitalmarketplace-admin': 'admin', 'GovWifi': 'push'}
+    "gds_metrics_python",  # {'digitalmarketplace': 'admin', 're-autom8': 'push'}
+    "aws-auth",  # {'digitalmarketplace': 'admin', 'notify': 'push'}
+}
+
 
 def get_repos_for_team(team_name):
     output = subprocess.run(
@@ -39,17 +54,17 @@ def get_team_permissions_for_repo(repo_name):
 
 
 if __name__ == "__main__":
-    admin_repos = get_repos_for_team("digitalmarketplace-admin")
+    admin_repos = get_repos_for_team("digitalmarketplace-admin") - REPOS_NOT_TO_MIGRATE
 
     print(admin_repos)
     print(len(admin_repos))
 
-    non_admin_repos = get_repos_for_team("digitalmarketplace") - admin_repos
+    non_admin_repos = get_repos_for_team("digitalmarketplace") - admin_repos - REPOS_NOT_TO_MIGRATE
 
     print(non_admin_repos)
     print(len(non_admin_repos))
 
-    read_only_repos = get_repos_for_team("digitalmarketplace-readonly") - admin_repos - non_admin_repos
+    read_only_repos = get_repos_for_team("digitalmarketplace-readonly") - admin_repos - non_admin_repos - REPOS_NOT_TO_MIGRATE
     if read_only_repos:
         raise Exception("There should be no repos accessible only to the read-only team")
 
