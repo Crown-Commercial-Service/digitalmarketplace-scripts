aa6efa708ea38f9fba674db084fb204be2b1655f
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-09-02 15:00:43 +0100

    Get closing date from API
    Get the date when the previous framework closed from the API. This should make it easier for the person running this script.

--- scripts/framework-applications/export-mailchimp-coming-emails.py
+++ scripts/framework-applications/export-mailchimp-coming-emails.py
@@ -3,24 +3,26 @@
 Get all the emails from mailchimp for people who've asked to be notified about new framework iterations.
 
 Usage:
-    /export-mailchimp-coming-emails.py <stage> <since> [--verbose]
+    /export-mailchimp-coming-emails.py <stage> <previous_framework> [--verbose]
 
 Parameters:
-    <stage>  Stage to target
-    <since>  The date when the previous framework closed for applications. In ISO format: YYYY-MM-DD
+    <stage>               Stage to target
+    <previous_framework>  The previous framework in this family
 """
 import csv
 import logging
 import sys
 
 from dateutil.parser import isoparse
+from dmapiclient import DataAPIClient
 from dmutils.email.dm_mailchimp import DMMailChimpClient
+from dmutils.env_helpers import get_api_endpoint_from_stage
 from docopt import docopt
 
 sys.path.insert(0, ".")
 
 from dmscripts.helpers import logging_helpers
-from dmscripts.helpers.auth_helpers import get_mailchimp_credentials
+from dmscripts.helpers.auth_helpers import get_mailchimp_credentials, get_auth_token
 
 TEST_AUDIENCE_ID = "18fb1fa411"  # PREVIEW OPEN_FRAMEWORK_NOTIFICATION_MAILING_LIST
 PRODUCTION_AUDIENCE_ID = "7534b3e89a"  # Digital Marketplace - open for applications
@@ -31,6 +33,14 @@ def output_emails_as_csv(email_addresses):
     writer.writerow(["email address"])
     for email_address in email_addresses:
         writer.writerow([email_address])
+
+
+def get_date_framework_closed(stage, framework_slug):
+    data_api_client = DataAPIClient(
+        get_api_endpoint_from_stage(stage), get_auth_token("api", stage)
+    )
+    previous_framework = data_api_client.get_framework(framework_slug)["frameworks"]
+    return isoparse(previous_framework["applicationsCloseAtUTC"])
 
 
 if __name__ == "__main__":
@@ -51,10 +61,11 @@ if __name__ == "__main__":
     )
 
     audience_id = PRODUCTION_AUDIENCE_ID if stage == "production" else TEST_AUDIENCE_ID
-    since = isoparse(arguments["<since>"])
     email_addresses = dm_mailchimp_client.get_email_addresses_from_list(
         audience_id,
         status="subscribed",
-        since_timestamp_opt=since.isoformat(),
+        since_timestamp_opt=get_date_framework_closed(
+            stage, arguments["<previous_framework>"]
+        ),
     )
     output_emails_as_csv(email_addresses)
