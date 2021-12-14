c0c1667f332f13c6ac28823b3e902fd5c7e31239
tberey <thomas.berey@crowncommercial.gov.uk>
2021-12-14 15:27:57 +0000

    This will find all users, that are a Supplier role and also active, that have been created since a given date

--- scripts/find-active-suppliers-since-a-date.py
+++ scripts/find-active-suppliers-since-a-date.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """
-This script was needed to find all active registered suppliers, since a given date.
+This will find all users, that are a Supplier and also active, that have been created since a given date.
 
-The script parses a date, provided by the user (if not default date of today is provided).
-This date is then compared against the Suppliers list, where active is true.
-If the date is more recent than the date the user provided, the record is captured and outputted.
-A count is taken and also provided.
+The script parses a date, provided by the user, (if not default today's date is provided as a default).
+This date is then taken and compared against the created dates of the returned looped list of suppliers,
+where active is true.
+If the creation date of the user is more recent than the compared date the user provided,
+the record is captured and outputted. A count is also taken and outputted.
 
 Usage:
     scripts/find-active-suppliers-since-a-date.py <stage> [options]
 
 Options:
-    --date=YYYY-MM-DD       Limit the number of suppliers to find [default: <today's date>]
+    --date=YYYY-MM-DD       Limit the number of suppliers to find [default: ]
 """
 import sys
 import os
@@ -26,19 +27,21 @@ sys.path.insert(0, ".")
 
 from dmscripts.helpers.auth_helpers import get_auth_token
 from dmscripts.helpers.updated_by_helpers import get_user
+from dmscripts.helpers.datetime_helpers import parse_datetime
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
     stage = arguments['<stage>']
-    user_date = arguments['--date']
 
     data_api_client = DataAPIClient(
         get_api_endpoint_from_stage(stage), get_auth_token("api", stage), user=get_user()
     )
 
+    date = arguments['--date'] or datetime.now().strftime("%Y-%m-%d")
     counter = 0
-    date_format = "%Y-%m-%d"
-    formatted_date = datetime.strptime(user_date, date_format) or datetime.now().strftime(date_format)
+
+    if not os.path.exists('data'):
+        os.makedirs('data')
 
     with open(os.path.join(
         'data',
@@ -48,9 +51,7 @@ if __name__ == "__main__":
         writer.writerow(["email_address", "name", "supplierName", "supplierID"])
 
         for user in data_api_client.find_users_iter(role='supplier'):
-            if user['active'] and datetime.strptime(
-                user['createdAt'].split('T', 1)[0], date_format
-            ) >= formatted_date:
+            if user['active'] and parse_datetime(user['createdAt'].split('T', 1)[0]) >= parse_datetime(date):
                 writer.writerow([
                     user['emailAddress'],
                     user['name'],
