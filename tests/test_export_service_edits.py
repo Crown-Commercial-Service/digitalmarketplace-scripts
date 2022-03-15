f0783c51c8f816a9e952658c85de7682cef06fde
Benjamin Gill <benjamin.gill@crowncommercial.gov.uk>
2022-03-15 16:46:20 +0000

    Test diffing archived services
    I wasn't confident that the diffing code was diffing the right way round. Add tests to confirm the behaviour.
    
    It turns out that there is indeed a bug - we've got the diff reversed. These tests will help prove that my fix will be correct.

---
+++ tests/test_export_service_edits.py
@@ -0,0 +1,42 @@
+from dmapiclient import DataAPIClient
+from unittest import mock
+
+from dmscripts.export_service_edits import diff_archived_services, service_edit_diff
+
+
+class TestArchivedServiceDiff:
+    def test_no_content(self):
+        new_service = {"frameworkFamily": "foo", "lot": "bar"}
+        old_service = {"frameworkFamily": "foo", "lot": "bar"}
+
+        assert diff_archived_services(new_service, old_service) == ""
+
+    def test_diff_string_change(self):
+        new_service = {"frameworkFamily": "foo", "lot": "bar", "key": "new value"}
+        old_service = {"frameworkFamily": "foo", "lot": "bar", "key": "value"}
+
+        assert diff_archived_services(new_service, old_service) == """key:
+- value
++ new value
+? ++++
+"""
+
+
+class TestServiceEditDiff:
+    def test_diff(self):
+        data_api_client = mock.Mock(autospec=DataAPIClient)
+        data_api_client.get_archived_service.side_effect = [
+            {"frameworkFamily": "foo", "lot": "bar", "key": "new value"},
+            {"frameworkFamily": "foo", "lot": "bar", "key": "value"},
+        ]
+
+        audit_event = {
+            "type": "update_service",
+            "data": {"newArchivedServiceId": 1, "oldArchivedServiceId": 2}
+        }
+        assert service_edit_diff(data_api_client, audit_event) == """key:
+- value
++ new value
+? ++++
+"""
+        data_api_client.assert_has_calls([mock.call.get_archived_service(1), mock.call.get_archived_service(2)])
